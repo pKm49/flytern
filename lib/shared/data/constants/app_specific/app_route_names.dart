770c9ed807d29964f8df6406365a836bcfb82a53
Prasanth K M <prasanthkm49@gmail.com>
2023-09-28 18:38:29 +0530

    added valid app route names

---
+++ lib/shared/data/constants/app_specific/app_route_names.dart
@@ -0,0 +1,13 @@
+//language selection
+  const String Approute_langaugeSelector = '/language-selector';
+
+//authenticaion
+  const String Approute_auth = '/auth';
+  const String Approute_authSelector  = '$Approute_auth/auth-selector';
+  const String Approute_registerMobile = '$Approute_auth/register-mobile';
+  const String Approute_registerOtp = '$Approute_auth/register-otp';
+  const String Approute_registerPersonalData = '$Approute_auth/register-personal-data';
+  const String Approute_login  = '$Approute_auth/login';
+  const String Approute_resetPasswordMobile = '$Approute_auth/reset-password-mobile';
+  const String Approute_resetPasswordOtp = '$Approute_auth/reset-password-otp';
+  const String Approute_resetPasswordNewpassword = '$Approute_auth/reset-password-new-password';
