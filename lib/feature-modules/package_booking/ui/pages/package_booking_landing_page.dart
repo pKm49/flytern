13ca76743184e60f8d05de1adeab75dd5f20dc00
Prasanth K M <prasanthkm49@gmail.com>
2023-09-29 18:32:24 +0530

    added package booking landing page with statefull widget

---
+++ lib/feature-modules/package_booking/ui/pages/package_booking_landing_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class PackageBookingLandingPage extends StatefulWidget {
+  const PackageBookingLandingPage({super.key});
+
+  @override
+  State<PackageBookingLandingPage> createState() => _PackageBookingLandingPageState();
+}
+
+class _PackageBookingLandingPageState extends State<PackageBookingLandingPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
