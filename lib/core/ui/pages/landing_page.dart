9c7eee6a751660117d6ef0694c9b41514b794c85
Prasanth K M <prasanthkm49@gmail.com>
2023-09-29 18:25:19 +0530

    added landing page with statefull widget

---
+++ lib/core/ui/pages/landing_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class CoreLandingPage extends StatefulWidget {
+  const CoreLandingPage({super.key});
+
+  @override
+  State<CoreLandingPage> createState() => _CoreLandingPageState();
+}
+
+class _CoreLandingPageState extends State<CoreLandingPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
