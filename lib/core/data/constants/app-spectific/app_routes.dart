ad93024f39cea1e4a2a857cacf83867b6ad5aa6b
Prasanth K M <prasanthkm49@gmail.com>
2023-09-29 08:57:37 +0530

    added auth routes

--- lib/core/data/constants/app-spectific/app_routes.dart
+++ lib/core/data/constants/app-spectific/app_routes.dart
@@ -1,6 +1,12 @@
 
 import 'package:flytern/core/ui/pages/language_selector.dart';
 import 'package:flytern/feature-modules/auth/ui/pages/auth_selector.dart';
+import 'package:flytern/feature-modules/auth/ui/pages/login.dart';
+import 'package:flytern/feature-modules/auth/ui/pages/register/register_details_input.dart';
+import 'package:flytern/feature-modules/auth/ui/pages/register/register_otp_input.dart';
+import 'package:flytern/feature-modules/auth/ui/pages/reset_password/reset_password_credentials.dart';
+import 'package:flytern/feature-modules/auth/ui/pages/reset_password/reset_password_new_password.dart';
+import 'package:flytern/feature-modules/auth/ui/pages/reset_password/reset_password_otp.dart';
 import 'package:flytern/shared/data/constants/app_specific/app_route_names.dart';
 import 'package:get/get.dart';
 
@@ -14,6 +20,36 @@ getAppRoutes() => [
   GetPage(
     name: Approute_authSelector,
     page: () => const AuthSelectorPage(),
+    middlewares: [MyMiddelware()],
+  ),
+  GetPage(
+    name: Approute_registerMobile,
+    page: () => const AuthRegisterDetailsInputPage(),
+    middlewares: [MyMiddelware()],
+  ),
+  GetPage(
+    name: Approute_registerOtp,
+    page: () => const AuthRegisterOTPInputPage(),
+    middlewares: [MyMiddelware()],
+  ),
+  GetPage(
+    name: Approute_login,
+    page: () => const AuthLoginPage(),
+    middlewares: [MyMiddelware()],
+  ),
+  GetPage(
+    name: Approute_resetPasswordMobile,
+    page: () => const AuthResetPasswordCredentialsPage(),
+    middlewares: [MyMiddelware()],
+  ),
+  GetPage(
+    name: Approute_resetPasswordOtp,
+    page: () => const AuthResetPasswordOTPPage(),
+    middlewares: [MyMiddelware()],
+  ),
+  GetPage(
+    name: Approute_resetPasswordNewpassword,
+    page: () => const AuthResetPasswordNewPasswordPage(),
     middlewares: [MyMiddelware()],
   ),
 ];
