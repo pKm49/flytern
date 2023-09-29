f63fb0d7014dd1d0ba736037ba1dea19e3ab2aed
Prasanth K M <prasanthkm49@gmail.com>
2023-09-29 18:30:12 +0530

    added activity booking landing page with statefull widget

---
+++ lib/feature-modules/activity_booking/ui/pages/activity_booking_landing_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ActivityBookingLandingPage extends StatefulWidget {
+  const ActivityBookingLandingPage({super.key});
+
+  @override
+  State<ActivityBookingLandingPage> createState() => _ActivityBookingLandingPageState();
+}
+
+class _ActivityBookingLandingPageState extends State<ActivityBookingLandingPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
