4318440c7e314a271ffe9cf8104c814ce3cdcc20
Prasanth K M <prasanthkm49@gmail.com>
2023-09-29 18:32:31 +0530

    added profile landing page with statefull widget

---
+++ lib/feature-modules/profile/ui/pages/profile_landing_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ProfileLandingPage extends StatefulWidget {
+  const ProfileLandingPage({super.key});
+
+  @override
+  State<ProfileLandingPage> createState() => _ProfileLandingPageState();
+}
+
+class _ProfileLandingPageState extends State<ProfileLandingPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
