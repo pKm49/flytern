835744602fd34437ff0d7db2b12a0c8c59ccf523
Prasanth K M <prasanthkm49@gmail.com>
2023-09-29 18:32:16 +0530

    added hotel booking landing page with statefull widget

---
+++ lib/feature-modules/hotel_booking/ui/pages/hotel_booking_landing_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class HotelBookingLandingPage extends StatefulWidget {
+  const HotelBookingLandingPage({super.key});
+
+  @override
+  State<HotelBookingLandingPage> createState() => _HotelBookingLandingPageState();
+}
+
+class _HotelBookingLandingPageState extends State<HotelBookingLandingPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
