3f695e545fca6ed8164430dec2ed7928353d7e2a
Prasanth K M <prasanthkm49@gmail.com>
2023-09-29 16:16:50 +0530

    updated UI

--- lib/feature-modules/auth/ui/pages/reset_password/reset_password_credentials.dart
+++ lib/feature-modules/auth/ui/pages/reset_password/reset_password_credentials.dart
@@ -1,4 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flytern/shared/data/constants/app_specific/app_route_names.dart';
+import 'package:flytern/shared/data/constants/ui_constants/style_params.dart';
+import 'package:flytern/shared/data/constants/ui_constants/widget_styles.dart';
+import 'package:flytern/shared/services/utility-services/form_validator.dart';
+import 'package:flytern/shared/services/utility-services/widget_generator.dart';
+import 'package:flytern/shared/services/utility-services/widget_properties_generator.dart';
+import 'package:get/get.dart';
 
 class AuthResetPasswordCredentialsPage extends StatefulWidget {
   const AuthResetPasswordCredentialsPage({super.key});
@@ -8,8 +15,49 @@ class AuthResetPasswordCredentialsPage extends StatefulWidget {
 }
 
 class _AuthResetPasswordCredentialsPageState extends State<AuthResetPasswordCredentialsPage> {
+  TextEditingController emailController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+  final GlobalKey<FormState> loginFormKey = GlobalKey<FormState>();
+
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+
+    double screenwidth = MediaQuery.of(context).size.width;
+    double screenheight = MediaQuery.of(context).size.height;
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("reset_password".tr),
+      ),
+      body: Container(
+        width: screenwidth,
+        padding: flyternLargePaddingAll,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text("reset_password_message".tr,style: getBodyMediumStyle(context)),
+            addVerticalSpace(flyternSpaceLarge*2),
+            TextFormField(
+                controller: emailController,
+                validator: (value) => checkIfEmailValid(value),
+                keyboardType: TextInputType.number,
+                decoration: InputDecoration(
+                  labelText: "email".tr,
+                )),
+            addVerticalSpace(flyternSpaceLarge),
+            SizedBox(
+              width: double.infinity,
+              child: ElevatedButton(
+                  onPressed: () async {
+                    Get.toNamed(Approute_resetPasswordOtp);
+                  },
+                  child:Text("submit".tr )),
+            ),
+
+          ],
+        ),
+      ),
+    );
   }
 }
