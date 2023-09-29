bc45e4f89982d34a655c4e0cbf9aa6266800c737
Prasanth K M <prasanthkm49@gmail.com>
2023-09-29 18:30:49 +0530

    added flight booking landing page with statefull widget

---
+++ lib/feature-modules/flight_booking/ui/pages/flight_booking_landing_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class FlightBookingLandingPage extends StatefulWidget {
+  const FlightBookingLandingPage({super.key});
+
+  @override
+  State<FlightBookingLandingPage> createState() => _FlightBookingLandingPageState();
+}
+
+class _FlightBookingLandingPageState extends State<FlightBookingLandingPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
