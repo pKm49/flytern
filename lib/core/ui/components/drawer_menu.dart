312783a5b76ff11f1eda0d6ca7a200a334a5d260
Prasanth K M <prasanthkm49@gmail.com>
2023-09-29 19:15:00 +0530

    added drawer menu widget

---
+++ lib/core/ui/components/drawer_menu.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class CoreDrawerMenuPage extends StatefulWidget {
+  const CoreDrawerMenuPage({super.key});
+
+  @override
+  State<CoreDrawerMenuPage> createState() => _CoreDrawerMenuPageState();
+}
+
+class _CoreDrawerMenuPageState extends State<CoreDrawerMenuPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
