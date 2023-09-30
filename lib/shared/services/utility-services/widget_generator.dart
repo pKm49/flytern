b75d3ba4190f41e11418b3c8e49313817be2cc47
Prasanth K M <prasanthkm49@gmail.com>
2023-09-30 09:21:31 +0530

    added height and width for spaces

--- lib/shared/services/utility-services/widget_generator.dart
+++ lib/shared/services/utility-services/widget_generator.dart
@@ -3,11 +3,13 @@ import 'package:flutter/material.dart';
 Widget addVerticalSpace(double height){
   return SizedBox(
     height: height,
+    width: 1,
   );
 }
 
 Widget addHorizontalSpace(double width){
   return SizedBox(
     width: width,
+    height: 1,
   );
 }
