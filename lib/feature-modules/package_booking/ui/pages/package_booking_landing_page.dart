6d334d9b0fae14289c25b13df4e1dfa802e15500
Prasanth K M <prasanthkm49@gmail.com>
2023-09-30 11:31:54 +0530

    updated UI

--- lib/feature-modules/package_booking/ui/pages/package_booking_landing_page.dart
+++ lib/feature-modules/package_booking/ui/pages/package_booking_landing_page.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flytern/shared/data/constants/ui_constants/style_params.dart';
+import 'package:flytern/shared/data/constants/ui_constants/widget_styles.dart';
+import 'package:flytern/shared/services/utility-services/widget_generator.dart';
+import 'package:flytern/shared/ui/components/dropdown_selector.dart';
 
 class PackageBookingLandingPage extends StatefulWidget {
   const PackageBookingLandingPage({super.key});
@@ -10,6 +14,39 @@ class PackageBookingLandingPage extends StatefulWidget {
 class _PackageBookingLandingPageState extends State<PackageBookingLandingPage> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+
+    double screenwidth = MediaQuery.of(context).size.width;
+    double screenheight = MediaQuery.of(context).size.height;
+
+    return Container(
+      height: screenheight,
+      width: screenwidth,
+      color: flyternGrey20,
+      child: Column(
+        children: [
+          Container(
+            width: screenwidth-flyternSpaceLarge*2,
+            padding: flyternMediumPaddingHorizontal,
+            margin: flyternLargePaddingAll,
+            decoration: BoxDecoration(
+              color: flyternBackgroundWhite,
+              borderRadius: BorderRadius.circular(flyternBorderRadiusExtraSmall),
+            ),
+            child: DropDownSelector(
+              titleText: "Select Destination",
+              selected:null  ,
+              items: [],
+              hintText:"Select Destination",
+              valueChanged: (newZone) {
+
+              },
+            ),
+          ),
+          Expanded(child: ListView(
+            children: [],
+          ))
+        ],
+      ),
+    );
   }
 }
