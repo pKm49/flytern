8a9c173b069e29aa7c924c29fc2bd61685ba811b
Prasanth K M <prasanthkm49@gmail.com>
2023-09-30 09:39:16 +0530

    updated UI

--- lib/feature-modules/hotel_booking/ui/pages/hotel_booking_landing_page.dart
+++ lib/feature-modules/hotel_booking/ui/pages/hotel_booking_landing_page.dart
@@ -1,15 +1,282 @@
 import 'package:flutter/material.dart';
+import 'package:flytern/feature-modules/flight_booking/ui/components/flight_booking_form.dart';
+import 'package:flytern/feature-modules/flight_booking/ui/components/flight_type_tab.dart';
+import 'package:flytern/shared/data/constants/ui_constants/asset_urls.dart';
+import 'package:flytern/shared/data/constants/ui_constants/style_params.dart';
+import 'package:flytern/shared/data/constants/ui_constants/widget_styles.dart';
+import 'package:flytern/shared/services/utility-services/widget_generator.dart';
+import 'package:flytern/shared/services/utility-services/widget_properties_generator.dart';
+import 'package:ionicons/ionicons.dart';
 
 class HotelBookingLandingPage extends StatefulWidget {
   const HotelBookingLandingPage({super.key});
 
   @override
-  State<HotelBookingLandingPage> createState() => _HotelBookingLandingPageState();
+  State<HotelBookingLandingPage> createState() =>
+      _HotelBookingLandingPageState();
 }
 
-class _HotelBookingLandingPageState extends State<HotelBookingLandingPage> {
+class _HotelBookingLandingPageState extends State<HotelBookingLandingPage>
+    with SingleTickerProviderStateMixin {
+  int selectedTab = 1;
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    double screenwidth = MediaQuery.of(context).size.width;
+    double screenheight = MediaQuery.of(context).size.height;
+
+    return Container(
+      height: screenheight,
+      width: screenwidth,
+      decoration: BoxDecoration(
+        image: DecorationImage(
+          image: AssetImage(ASSETS_FLIGHTS_BG),
+          fit: BoxFit.cover,
+        ),
+      ),
+      child: ListView(
+        children: [
+          Container(
+            margin: EdgeInsets.all(flyternSpaceLarge),
+            width: screenwidth - (flyternSpaceLarge * 2),
+            decoration: flyternShadowedContainerSmallDecoration,
+            padding: flyternMediumPaddingAll,
+            child: Wrap(
+              children: [
+                Container(
+                  decoration: flyternBorderedContainerSmallDecoration.copyWith(
+                      border: Border.all(color: flyternGrey20, width: .5)),
+                  padding: flyternMediumPaddingAll,
+                  child: Row(
+                    children: [
+                      Icon(Ionicons.location_outline,
+                          color: flyternSecondaryColor,size: flyternFontSize20),
+                      addHorizontalSpace(flyternSpaceSmall*1.5),
+                      Expanded(
+                        flex: 1,
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              'Destination',
+                              style: getLabelLargeStyle(context).copyWith(
+                                  color: flyternGrey40,
+                                  fontWeight: FontWeight.  w400),
+                            ),
+                            addVerticalSpace(flyternSpaceExtraSmall*1.5),
+                            Text('Dubai',
+                                style: getLabelLargeStyle(context)
+                                    .copyWith(color: flyternGrey80, )),
+                          ],
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+                addVerticalSpace(flyternSpaceMedium),
+                Row(
+                  children: [
+                    Expanded(
+                        flex: 1,
+                        child: Container(
+                          decoration: flyternBorderedContainerSmallDecoration.copyWith(
+                              border: Border.all(color: flyternGrey20, width: .5)),
+                          padding: flyternMediumPaddingAll,
+                          child: Row(
+                            children: [
+                              Icon(Icons.calendar_month,
+                                  color: flyternSecondaryColor,size: flyternFontSize20),
+                              addHorizontalSpace(flyternSpaceSmall*1.5),
+                              Expanded(
+                                flex: 1,
+                                child: Column(
+                                  mainAxisAlignment: MainAxisAlignment.start,
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Text(
+                                      'Checkin',
+                                      style: getLabelLargeStyle(context).copyWith(
+                                          color: flyternGrey40,
+                                          fontWeight: FontWeight.  w400),
+                                    ),
+                                    addVerticalSpace(flyternSpaceExtraSmall*1.5),
+                                    Text('Jul 24, 2023',
+                                        style: getLabelLargeStyle(context)
+                                            .copyWith(color: flyternGrey80, )),
+                                  ],
+                                ),
+                              )
+                            ],
+                          ),
+                        )),
+                    addHorizontalSpace(flyternSpaceSmall),
+                    Expanded(
+                        flex: 1,
+                        child: Container(
+                          decoration: flyternBorderedContainerSmallDecoration.copyWith(
+                              border: Border.all(color: flyternGrey20, width: .5)),
+                          padding: flyternMediumPaddingAll ,
+                          child: Row(
+                            children: [
+                              Icon(Icons.calendar_month,
+                                  color: flyternSecondaryColor,size: flyternFontSize20),
+                              addHorizontalSpace(flyternSpaceSmall*1.5),
+                              Expanded(
+                                flex: 1,
+                                child: Column(
+                                  mainAxisAlignment: MainAxisAlignment.start,
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Text(
+                                      'Checkout',
+                                      style: getLabelLargeStyle(context).copyWith(
+                                          color: flyternGrey40,
+                                          fontWeight: FontWeight.  w400),
+                                    ),
+                                    addVerticalSpace(flyternSpaceExtraSmall*1.5),
+                                    Text('Jul 24, 2023',
+                                        style: getLabelLargeStyle(context)
+                                            .copyWith( color: flyternGrey80)),
+                                  ],
+                                ),
+                              )
+                            ],
+                          ),
+                        )),
+                  ],
+                ),
+                addVerticalSpace(flyternSpaceMedium),
+                Row(
+                  children: [
+                    Expanded(
+                        flex: 1,
+                        child: Container(
+                          decoration: flyternBorderedContainerSmallDecoration.copyWith(
+                              border: Border.all(color: flyternGrey20, width: .5)),
+                          padding: flyternMediumPaddingAll,
+                          child: Row(
+                            children: [
+                              Icon(Ionicons.bed_outline,
+                                  color: flyternSecondaryColor,size: flyternFontSize20),
+                              addHorizontalSpace(flyternSpaceSmall*1.5),
+                              Expanded(
+                                flex: 1,
+                                child: Column(
+                                  mainAxisAlignment: MainAxisAlignment.start,
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Text(
+                                      'Travel Date',
+                                      style: getLabelLargeStyle(context).copyWith(
+                                          color: flyternGrey40,
+                                          fontWeight: FontWeight.  w400),
+                                    ),
+                                    addVerticalSpace(flyternSpaceExtraSmall*1.5),
+                                    Text('Rooms',
+                                        style: getLabelLargeStyle(context)
+                                            .copyWith(color: flyternGrey80, )),
+                                  ],
+                                ),
+                              )
+                            ],
+                          ),
+                        )),
+                    addHorizontalSpace(flyternSpaceSmall),
+                    Expanded(
+                        flex: 1,
+                        child: Container(
+                          decoration: flyternBorderedContainerSmallDecoration.copyWith(
+                              border: Border.all(color: flyternGrey20, width: .5)),
+                          padding: flyternMediumPaddingAll ,
+                          child: Row(
+                            children: [
+                              Icon(Ionicons.person_outline,
+                                  color: flyternSecondaryColor,size: flyternFontSize20),
+                              addHorizontalSpace(flyternSpaceSmall*1.5),
+                              Expanded(
+                                flex: 1,
+                                child: Column(
+                                  mainAxisAlignment: MainAxisAlignment.start,
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Text(
+                                      'Users',
+                                      style: getLabelLargeStyle(context).copyWith(
+                                          color: flyternGrey40,
+                                          fontWeight: FontWeight.  w400),
+                                    ),
+                                    addVerticalSpace(flyternSpaceExtraSmall*1.5),
+                                    Text('1 Adult',
+                                        style: getLabelLargeStyle(context)
+                                            .copyWith( color: flyternGrey80)),
+                                  ],
+                                ),
+                              )
+                            ],
+                          ),
+                        )),
+                  ],
+                ),
+                addVerticalSpace(flyternSpaceMedium),
+
+                Container(
+                  decoration: flyternBorderedContainerSmallDecoration.copyWith(
+                      border: Border.all(color: flyternGrey20, width: .5)),
+                  padding: flyternMediumPaddingAll,
+                  child: Row(
+                    children: [
+                      Icon(Icons.flag_outlined,
+                          color: flyternSecondaryColor,size: flyternFontSize20),
+                      addHorizontalSpace(flyternSpaceSmall*1.5),
+                      Expanded(
+                        flex: 1,
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              'Nationality',
+                              style: getLabelLargeStyle(context).copyWith(
+                                  color: flyternGrey40,
+                                  fontWeight: FontWeight.  w400),
+                            ),
+                            addVerticalSpace(flyternSpaceExtraSmall*1.5),
+                            Text('Indian',
+                                style: getLabelLargeStyle(context)
+                                    .copyWith( color: flyternGrey80, )),
+                          ],
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+                addVerticalSpace(flyternSpaceMedium),
+
+                SizedBox(
+                  width: double.infinity,
+                  child: ElevatedButton(
+                      onPressed: ()   {
+
+                      }, child: Text("Search Hotels" )),
+                ),
+
+              ],
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
