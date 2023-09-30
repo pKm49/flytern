fa83cd5130b09b68b9a07c2182929be67af32d50
Prasanth K M <prasanthkm49@gmail.com>
2023-09-30 12:23:31 +0530

    updated UI

--- lib/feature-modules/activity_booking/ui/pages/activity_booking_landing_page.dart
+++ lib/feature-modules/activity_booking/ui/pages/activity_booking_landing_page.dart
@@ -1,4 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flytern/feature-modules/activity_booking/ui/components/activity_list_card.dart';
+import 'package:flytern/feature-modules/package_booking/ui/components/package_list_card.dart';
+import 'package:flytern/shared/data/constants/ui_constants/asset_urls.dart';
+import 'package:flytern/shared/data/constants/ui_constants/style_params.dart';
+import 'package:flytern/shared/data/constants/ui_constants/widget_styles.dart';
+import 'package:flytern/shared/data/models/store_models/general_item.dart';
+import 'package:flytern/shared/services/utility-services/widget_generator.dart';
+import 'package:flytern/shared/ui/components/dropdown_selector.dart';
 
 class ActivityBookingLandingPage extends StatefulWidget {
   const ActivityBookingLandingPage({super.key});
@@ -10,6 +18,105 @@ class ActivityBookingLandingPage extends StatefulWidget {
 class _ActivityBookingLandingPageState extends State<ActivityBookingLandingPage> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+
+    double screenwidth = MediaQuery.of(context).size.width;
+    double screenheight = MediaQuery.of(context).size.height;
+
+    return Container(
+      height: screenheight,
+      width: screenwidth,
+      color: flyternGrey10,
+      child: Column(
+        children: [
+          Container(
+            width: screenwidth-flyternSpaceMedium*2,
+            padding: flyternMediumPaddingHorizontal,
+            margin: flyternMediumPaddingAll,
+            decoration: BoxDecoration(
+              color: flyternBackgroundWhite,
+              boxShadow: flyternItemShadow,
+              borderRadius: BorderRadius.circular(flyternBorderRadiusExtraSmall),
+            ),
+            child: DropDownSelector(
+              titleText: "Select Destination",
+              selected:null  ,
+              items: [
+                GeneralItem.GeneralItem(id: 1, name: "Select Destination", arabicName: "Select Destination"),
+                GeneralItem.GeneralItem(id: 2, name: "India", arabicName: "India"),
+                GeneralItem.GeneralItem(id: 3, name: "Spain", arabicName: "Spain"),
+                GeneralItem.GeneralItem(id: 4, name: "Nepal", arabicName: "Nepal"),
+              ],
+              hintText:"Select Destination",
+              valueChanged: (newZone) {
+
+              },
+            ),
+          ),
+          Expanded(child: Container(
+            color: flyternBackgroundWhite,
+            child: ListView(
+              children: [
+                ActivityListCard(
+                  imageUrl: ASSETS_PACKAGE_1_SAMPLE,
+                  title: 'Shrek\'s Adventure',
+                  flightName: 'Ticket (PP)',
+                  hotelName: 'The Bank Hotel',
+                  sponsoredBy: 'Central London',
+                  price: 15000,
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: flyternSpaceMedium),
+                  child: Divider(),
+                ),
+                ActivityListCard(
+                  imageUrl: ASSETS_PACKAGE_1_SAMPLE,
+                  title: 'Shrek\'s Adventure',
+                  flightName: 'KBL to IST',
+                  hotelName: 'The Bank Hotel',
+                  sponsoredBy: 'Central London',
+                  price: 15000,
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: flyternSpaceMedium),
+                  child: Divider(),
+                ),
+                ActivityListCard(
+                  imageUrl: ASSETS_PACKAGE_1_SAMPLE,
+                  title: 'Shrek\'s Adventure',
+                  flightName: 'KBL to IST',
+                  hotelName: 'The Bank Hotel',
+                  sponsoredBy: 'Central London',
+                  price: 15000,
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: flyternSpaceMedium),
+                  child: Divider(),
+                ),
+                ActivityListCard(
+                  imageUrl: ASSETS_PACKAGE_1_SAMPLE,
+                  title: 'Shrek\'s Adventure',
+                  flightName: 'KBL to IST',
+                  hotelName: 'The Bank Hotel',
+                  sponsoredBy: 'Central London',
+                  price: 15000,
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: flyternSpaceMedium),
+                  child: Divider(),
+                ),
+                ActivityListCard(
+                  imageUrl: ASSETS_PACKAGE_1_SAMPLE,
+                  title: 'Shrek\'s Adventure',
+                  flightName: 'KBL to IST',
+                  hotelName: 'The Bank Hotel',
+                  sponsoredBy: 'Central London',
+                  price: 15000,
+                ),
+              ],
+            ),
+          ))
+        ],
+      ),
+    );
   }
 }
