8557893dae0311516f1ee4536d3858d31b8753e2
Prasanth K M <prasanthkm49@gmail.com>
2023-09-30 12:15:23 +0530

    updated dropdown width, margin & shadow. Added sample items

--- lib/feature-modules/package_booking/ui/pages/package_booking_landing_page.dart
+++ lib/feature-modules/package_booking/ui/pages/package_booking_landing_page.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flytern/feature-modules/package_booking/ui/components/package_list_card.dart';
+import 'package:flytern/shared/data/constants/ui_constants/asset_urls.dart';
 import 'package:flytern/shared/data/constants/ui_constants/style_params.dart';
 import 'package:flytern/shared/data/constants/ui_constants/widget_styles.dart';
+import 'package:flytern/shared/data/models/store_models/general_item.dart';
 import 'package:flytern/shared/services/utility-services/widget_generator.dart';
 import 'package:flytern/shared/ui/components/dropdown_selector.dart';
 
@@ -21,29 +24,95 @@ class _PackageBookingLandingPageState extends State<PackageBookingLandingPage> {
     return Container(
       height: screenheight,
       width: screenwidth,
-      color: flyternGrey20,
+      color: flyternGrey10,
       child: Column(
         children: [
           Container(
-            width: screenwidth-flyternSpaceLarge*2,
+            width: screenwidth-flyternSpaceMedium*2,
             padding: flyternMediumPaddingHorizontal,
-            margin: flyternLargePaddingAll,
+            margin: flyternMediumPaddingAll,
             decoration: BoxDecoration(
               color: flyternBackgroundWhite,
+              boxShadow: flyternItemShadow,
               borderRadius: BorderRadius.circular(flyternBorderRadiusExtraSmall),
             ),
             child: DropDownSelector(
               titleText: "Select Destination",
               selected:null  ,
-              items: [],
+              items: [
+                GeneralItem.GeneralItem(id: 1, name: "Select Destination", arabicName: "Select Destination"),
+                GeneralItem.GeneralItem(id: 2, name: "India", arabicName: "India"),
+                GeneralItem.GeneralItem(id: 3, name: "Spain", arabicName: "Spain"),
+                GeneralItem.GeneralItem(id: 4, name: "Nepal", arabicName: "Nepal"),
+              ],
               hintText:"Select Destination",
               valueChanged: (newZone) {
 
               },
             ),
           ),
-          Expanded(child: ListView(
-            children: [],
+          Expanded(child: Container(
+            color: flyternBackgroundWhite,
+            child: ListView(
+              children: [
+                PackageListCard(
+                  imageUrl: ASSETS_PACKAGE_1_SAMPLE,
+                  title: 'Kabul Holiday Package',
+                  flightName: 'KBL to IST',
+                  hotelName: 'The Bank Hotel',
+                  sponsoredBy: 'Emirates',
+                  price: 15000,
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: flyternSpaceMedium),
+                  child: Divider(),
+                ),
+                PackageListCard(
+                  imageUrl: ASSETS_PACKAGE_1_SAMPLE,
+                  title: 'Kabul Holiday Package',
+                  flightName: 'KBL to IST',
+                  hotelName: 'The Bank Hotel',
+                  sponsoredBy: 'Emirates',
+                  price: 15000,
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: flyternSpaceMedium),
+                  child: Divider(),
+                ),
+                PackageListCard(
+                  imageUrl: ASSETS_PACKAGE_1_SAMPLE,
+                  title: 'Kabul Holiday Package',
+                  flightName: 'KBL to IST',
+                  hotelName: 'The Bank Hotel',
+                  sponsoredBy: 'Emirates',
+                  price: 15000,
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: flyternSpaceMedium),
+                  child: Divider(),
+                ),
+                PackageListCard(
+                  imageUrl: ASSETS_PACKAGE_1_SAMPLE,
+                  title: 'Kabul Holiday Package',
+                  flightName: 'KBL to IST',
+                  hotelName: 'The Bank Hotel',
+                  sponsoredBy: 'Emirates',
+                  price: 15000,
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: flyternSpaceMedium),
+                  child: Divider(),
+                ),
+                PackageListCard(
+                  imageUrl: ASSETS_PACKAGE_1_SAMPLE,
+                  title: 'Kabul Holiday Package',
+                  flightName: 'KBL to IST',
+                  hotelName: 'The Bank Hotel',
+                  sponsoredBy: 'Emirates',
+                  price: 15000,
+                ),
+              ],
+            ),
           ))
         ],
       ),
