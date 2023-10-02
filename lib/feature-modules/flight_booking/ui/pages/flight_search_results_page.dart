833ad7ad2ce4a41e767e85e57f9418b51ccb0079
Prasanth K M <prasanthkm49@gmail.com>
2023-10-02 23:44:29 +0530

    added search params

--- lib/feature-modules/flight_booking/ui/pages/flight_search_results_page.dart
+++ lib/feature-modules/flight_booking/ui/pages/flight_search_results_page.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flytern/shared/data/constants/ui_constants/style_params.dart';
+import 'package:flytern/shared/data/constants/ui_constants/widget_styles.dart';
 import 'package:flytern/shared/services/utility-services/widget_generator.dart';
+import 'package:flytern/shared/services/utility-services/widget_properties_generator.dart';
 import 'package:get/get.dart';
 import 'package:ionicons/ionicons.dart';
 
@@ -15,6 +17,10 @@ class FlightSearchResultPage extends StatefulWidget {
 class _FlightSearchResultPageState extends State<FlightSearchResultPage> {
   @override
   Widget build(BuildContext context) {
+
+    double screenwidth = MediaQuery.of(context).size.width;
+    double screenheight = MediaQuery.of(context).size.height;
+
     return Scaffold(
       appBar: AppBar(
         title: Text("search_results".tr),
@@ -22,6 +28,21 @@ class _FlightSearchResultPageState extends State<FlightSearchResultPage> {
           Icon(Ionicons.create_outline),
           addHorizontalSpace(flyternSpaceMedium),
         ],
+      ),
+      body: Container(
+        width: screenwidth,
+        height: screenheight,
+        child: Column(
+          children: [
+            addVerticalSpace(flyternSpaceMedium),
+            Container(
+              padding: flyternMediumPaddingHorizontal,
+              child: Text('KWI-DXB - 04-06 July, 23 - Round Trip - 1 Adults 1 Child',style: getLabelLargeStyle(context).copyWith(fontWeight: flyternFontWeightLight,color: flyternGrey40)),
+            ),
+            addVerticalSpace(flyternSpaceSmall),
+            Divider()
+          ],
+        ),
       ),
     );
   }
