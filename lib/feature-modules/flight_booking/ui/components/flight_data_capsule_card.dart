04da9b8ac8b035f9255c2f29b3c3b090f96d4aaa
Prasanth K M <prasanthkm49@gmail.com>
2023-10-03 01:30:45 +0530

    added transparent_bg capsule card

---
+++ lib/feature-modules/flight_booking/ui/components/flight_data_capsule_card.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:flytern/shared/data/constants/ui_constants/style_params.dart';
+import 'package:flytern/shared/data/constants/ui_constants/widget_styles.dart';
+import 'package:flytern/shared/services/utility-services/widget_properties_generator.dart';
+
+class FlightDataCapsuleCard extends StatelessWidget {
+  String label;
+  int theme;
+
+    FlightDataCapsuleCard({super.key, required this.label,required this.theme});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: flyternExtraSmallPaddingAll,
+      decoration: BoxDecoration(
+        color: theme==1?flyternPrimaryColorBg:flyternSecondaryColorBg,
+        borderRadius: BorderRadius.circular(flyternBorderRadiusExtraSmall),
+      ),
+      child: Center(
+        child: Text(
+          label,style: getLabelLargeStyle(context).copyWith(color: theme ==1?flyternPrimaryColor:flyternSecondaryColor),
+        ),
+      ),
+    );
+  }
+}
