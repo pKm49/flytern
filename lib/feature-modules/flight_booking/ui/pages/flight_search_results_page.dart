6b5934f212cb86279bbd9e0eebe8d9d2a9908223
Prasanth K M <prasanthkm49@gmail.com>
2023-10-03 01:14:57 +0530

    updated ui till tabbar

--- lib/feature-modules/flight_booking/ui/pages/flight_search_results_page.dart
+++ lib/feature-modules/flight_booking/ui/pages/flight_search_results_page.dart
@@ -14,7 +14,23 @@ class FlightSearchResultPage extends StatefulWidget {
   State<FlightSearchResultPage> createState() => _FlightSearchResultPageState();
 }
 
-class _FlightSearchResultPageState extends State<FlightSearchResultPage> {
+class _FlightSearchResultPageState extends State<FlightSearchResultPage> with SingleTickerProviderStateMixin{
+
+  late TabController tabController;
+
+  @override
+  void initState() {
+    super.initState();
+    tabController = TabController(vsync: this, length: 3, initialIndex: 0);
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
+    tabController.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
 
@@ -37,10 +53,128 @@ class _FlightSearchResultPageState extends State<FlightSearchResultPage> {
             addVerticalSpace(flyternSpaceMedium),
             Container(
               padding: flyternMediumPaddingHorizontal,
-              child: Text('KWI-DXB - 04-06 July, 23 - Round Trip - 1 Adults 1 Child',style: getLabelLargeStyle(context).copyWith(fontWeight: flyternFontWeightLight,color: flyternGrey40)),
+              child: Text('KWI-DXB - 04-06 July, 23 - Round Trip - 1 Adults 1 Child',
+                  textAlign: TextAlign.start,
+                  style: getLabelLargeStyle(context).copyWith(
+                      fontWeight: flyternFontWeightLight,color: flyternGrey40)),
             ),
             addVerticalSpace(flyternSpaceSmall),
-            Divider()
+            Divider(),
+            addVerticalSpace(flyternSpaceSmall),
+            Container(
+              padding: flyternMediumPaddingHorizontal,
+              child:  Row(
+                children: [
+                  Expanded(child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                           Text("sort".tr,style: getLabelLargeStyle(context).copyWith(fontWeight: flyternFontWeightLight,color: flyternGrey40)),
+                          addVerticalSpace(flyternSpaceExtraSmall),
+                          Text("price".tr,style: getBodyMediumStyle(context).copyWith(color: flyternGrey80)),
+
+                        ],
+                      ),
+                      Icon(Ionicons.chevron_down,color: flyternGrey40)
+                    ],
+                  )),
+                  addHorizontalSpace(flyternSpaceLarge),
+                  Expanded(child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                           Text("filter".tr,style: getLabelLargeStyle(context).copyWith(fontWeight: flyternFontWeightLight,color: flyternGrey40)),
+                          addVerticalSpace(flyternSpaceExtraSmall),
+                          Text("all".tr,style: getBodyMediumStyle(context).copyWith(color: flyternGrey80)),
+
+                        ],
+                      ),
+                      Icon(Ionicons.chevron_down,color: flyternGrey40)
+                    ],
+                  ))
+                ],
+              ),
+            ),
+            addVerticalSpace(flyternSpaceSmall),
+            Divider(),
+            Container(
+              padding: flyternMediumPaddingHorizontal,
+              decoration: BoxDecoration(
+                border: flyternDefaultBorderBottomOnly
+              ),
+              child:  TabBar(
+                  indicatorSize: TabBarIndicatorSize.tab,
+                  indicatorPadding: EdgeInsets.zero,
+                  labelPadding: EdgeInsets.zero,
+                  // indicator: new BubbleTabIndicator(
+                  //   indicatorHeight: 30.0,
+                  //   indicatorColor: AppColors.PrimaryColor,
+                  //   tabBarIndicatorSize: TabBarIndicatorSize.tab,
+                  // ),
+                  indicatorColor: flyternSecondaryColor,
+                  indicatorWeight: 2,
+                  padding: EdgeInsets.zero,
+                  controller: tabController,
+                  labelColor: flyternSecondaryColor,
+                  labelStyle: TextStyle(color:flyternPrimaryColor,fontWeight: FontWeight.bold ),
+                  unselectedLabelColor: flyternGrey20,
+                  tabs: <Container>[
+                      Container(
+                      padding:flyternExtraSmallPaddingVertical,
+                      child: Wrap(
+                        crossAxisAlignment: WrapCrossAlignment.center,
+                        direction: Axis.vertical,
+                        spacing: flyternSpaceExtraSmall,
+                        children: [
+                          Text("Tue July 4, 2023",style: getLabelLargeStyle(context).copyWith(
+                            color: tabController.index == 0?flyternSecondaryColor:flyternGrey40
+                          )),
+                          Text("AED 15,000",style: getBodyMediumStyle(context).copyWith(
+                              fontWeight: flyternFontWeightBold,
+                          color: tabController.index == 0?flyternSecondaryColor:flyternGrey40),),
+                        ],
+                      ),
+                    ),
+                      Container(
+                      padding:flyternExtraSmallPaddingVertical,
+                      child: Wrap(
+                        crossAxisAlignment: WrapCrossAlignment.center,
+                        direction: Axis.vertical,
+                        spacing: flyternSpaceExtraSmall,
+                        children: [
+                          Text("Wed July 5, 2023",style: getLabelLargeStyle(context).copyWith(
+                            color: tabController.index == 1?flyternSecondaryColor:flyternGrey40
+                          )),
+                          Text("AED 15,000",style: getBodyMediumStyle(context).copyWith(
+                              fontWeight: flyternFontWeightBold,
+                          color: tabController.index == 1?flyternSecondaryColor:flyternGrey40),),
+                        ],
+                      ),
+                    ),
+                      Container(
+                      padding:flyternExtraSmallPaddingVertical,
+                      child: Wrap(
+                        direction: Axis.vertical,
+                        spacing: flyternSpaceExtraSmall,
+                        crossAxisAlignment: WrapCrossAlignment.center,
+                        children: [
+                          Text("Thu July 6, 2023",style: getLabelLargeStyle(context).copyWith(
+                            color: tabController.index == 2?flyternSecondaryColor:flyternGrey40
+                          )),
+                          Text("AED 15,000",style: getBodyMediumStyle(context).copyWith(
+                              fontWeight: flyternFontWeightBold,
+                          color: tabController.index == 2?flyternSecondaryColor:flyternGrey40),),
+                        ],
+                      ),
+                    ),
+                  ])
+            ),
           ],
         ),
       ),
