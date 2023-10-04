478bd8080cd7849fb5e2341828b800291da18adf
Prasanth K M <prasanthkm49@gmail.com>
2023-10-04 14:21:47 +0530

    added page for seat selection in flightbooking

---
+++ lib/feature-modules/flight_booking/ui/pages/flight_seat_selection_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class FlightSeatSelectionPage extends StatefulWidget {
+  const FlightSeatSelectionPage({super.key});
+
+  @override
+  State<FlightSeatSelectionPage> createState() => _FlightSeatSelectionPageState();
+}
+
+class _FlightSeatSelectionPageState extends State<FlightSeatSelectionPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
