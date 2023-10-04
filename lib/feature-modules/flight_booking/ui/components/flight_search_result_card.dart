9c794336507d04d67d7c5dcec17de15ec82bce0d
Prasanth K M <prasanthkm49@gmail.com>
2023-10-04 17:54:37 +0530

    added navigation to flight_details

--- lib/feature-modules/flight_booking/ui/components/flight_search_result_card.dart
+++ lib/feature-modules/flight_booking/ui/components/flight_search_result_card.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flytern/feature-modules/flight_booking/ui/components/flight_airport_lable_card.dart';
 import 'package:flytern/feature-modules/flight_booking/ui/components/flight_data_capsule_card.dart';
+import 'package:flytern/shared/data/constants/app_specific/app_route_names.dart';
 import 'package:flytern/shared/data/constants/ui_constants/asset_urls.dart';
 import 'package:flytern/shared/data/constants/ui_constants/style_params.dart';
 import 'package:flytern/shared/data/constants/ui_constants/widget_styles.dart';
@@ -115,6 +116,8 @@ class FlightSearchResultCard extends StatelessWidget {
                             vertical: flyternSpaceSmall)),
                   ),
                     onPressed: ()   {
+                      Get.toNamed(Approute_flightsDetails);
+
                     }, child: Text("select".tr)),
               ),
             ],
