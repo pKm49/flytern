169445bf7a42ca124048b46205bc21278eaf36af
Prasanth K M <prasanthkm49@gmail.com>
2023-10-04 14:14:33 +0530

    added page for selecting baggages for flightbooking

---
+++ lib/feature-modules/flight_booking/ui/pages/flight_baggage_selection_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class FlightBaggageSelectionPage extends StatefulWidget {
+  const FlightBaggageSelectionPage({super.key});
+
+  @override
+  State<FlightBaggageSelectionPage> createState() => _FlightBaggageSelectionPageState();
+}
+
+class _FlightBaggageSelectionPageState extends State<FlightBaggageSelectionPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
