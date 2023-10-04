6affc5cec964b75b580c533108f3da468528c924
Prasanth K M <prasanthkm49@gmail.com>
2023-10-04 14:16:15 +0530

    added page for booking summary of flightbooking

---
+++ lib/feature-modules/flight_booking/ui/pages/flight_booking_summary_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class FlightBookingSummaryPage extends StatefulWidget {
+  const FlightBookingSummaryPage({super.key});
+
+  @override
+  State<FlightBookingSummaryPage> createState() => _FlightBookingSummaryPageState();
+}
+
+class _FlightBookingSummaryPageState extends State<FlightBookingSummaryPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
