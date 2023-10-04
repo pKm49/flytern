32a67de4489410f5e2a8d2b07ee1d3fd0ae93718
Prasanth K M <prasanthkm49@gmail.com>
2023-10-05 03:35:47 +0530

    added package userdata submission page

---
+++ lib/feature-modules/package_booking/ui/pages/package_user_details_submission_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class PackageUserDetailsSubmissionPage extends StatefulWidget {
+  const PackageUserDetailsSubmissionPage({super.key});
+
+  @override
+  State<PackageUserDetailsSubmissionPage> createState() => _PackageUserDetailsSubmissionPageState();
+}
+
+class _PackageUserDetailsSubmissionPageState extends State<PackageUserDetailsSubmissionPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
