66ed4dce3ea0d94087cbf79370d067336826d0fa
Prasanth K M <prasanthkm49@gmail.com>
2023-10-04 14:21:41 +0530

    added page for meal selection in flightbooking

---
+++ lib/feature-modules/flight_booking/ui/pages/flight_meal_selection_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class FlightMealSelectionPage extends StatefulWidget {
+  const FlightMealSelectionPage({super.key});
+
+  @override
+  State<FlightMealSelectionPage> createState() => _FlightMealSelectionPageState();
+}
+
+class _FlightMealSelectionPageState extends State<FlightMealSelectionPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
