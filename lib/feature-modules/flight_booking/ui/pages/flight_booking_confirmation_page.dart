4422d5c85ccf9d21b5fa2c3906bfe8e12a9f9fde
Prasanth K M <prasanthkm49@gmail.com>
2023-10-04 14:16:00 +0530

    added page for booking confirmation of flightbooking

---
+++ lib/feature-modules/flight_booking/ui/pages/flight_booking_confirmation_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class FlightBookingConfirmationPage extends StatefulWidget {
+  const FlightBookingConfirmationPage({super.key});
+
+  @override
+  State<FlightBookingConfirmationPage> createState() => _FlightBookingConfirmationPageState();
+}
+
+class _FlightBookingConfirmationPageState extends State<FlightBookingConfirmationPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
