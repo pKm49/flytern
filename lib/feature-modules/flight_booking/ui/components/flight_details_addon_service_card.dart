4c998f5eaae9c05eac50aa54b9a2b0b9b7d3d47e
Prasanth K M <prasanthkm49@gmail.com>
2023-10-04 19:44:52 +0530

    added flight_details_addon_service_card

---
+++ lib/feature-modules/flight_booking/ui/components/flight_details_addon_service_card.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+import 'package:flytern/feature-modules/flight_booking/ui/components/flight_airport_lable_card.dart';
+import 'package:flytern/feature-modules/flight_booking/ui/components/flight_data_capsule_card.dart';
+import 'package:flytern/shared/data/constants/app_specific/app_route_names.dart';
+import 'package:flytern/shared/data/constants/ui_constants/asset_urls.dart';
+import 'package:flytern/shared/data/constants/ui_constants/style_params.dart';
+import 'package:flytern/shared/data/constants/ui_constants/widget_styles.dart';
+import 'package:flytern/shared/services/utility-services/widget_generator.dart';
+import 'package:flytern/shared/services/utility-services/widget_properties_generator.dart';
+import 'package:get/get.dart';
+
+class FlightDetailsAddonServiceCard extends StatelessWidget {
+  String ImageUrl;
+  String keyLabel;
+  String valueLabel;
+
+    FlightDetailsAddonServiceCard({super.key, required this.ImageUrl, required this.keyLabel,
+    required this.valueLabel});
+
+  @override
+  Widget build(BuildContext context) {
+    double screenwidth = MediaQuery.of(context).size.width;
+    double screenheight = MediaQuery.of(context).size.height;
+
+    return Container(
+      color: flyternBackgroundWhite,
+      padding: flyternMediumPaddingAll,
+      width: screenwidth ,
+      child: Wrap(
+        children: [
+          Row(
+            children: [
+              Expanded(
+                  flex: 1,
+                  child: Image.asset(ImageUrl)),
+              addHorizontalSpace(flyternSpaceMedium),
+              Expanded(
+                  flex: 5,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(keyLabel,style: getBodyMediumStyle(context).copyWith(color: flyternGrey40),),
+                      addVerticalSpace(flyternSpaceExtraSmall),
+                      Text(valueLabel,style: getBodyMediumStyle(context).copyWith(color: flyternGrey80),),
+
+                    ],
+                  ))
+            ],
+          )
+        ],
+      )
+    );
+  }
+}
