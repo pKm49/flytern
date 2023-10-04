434614e8a7c4aeb80dd2ed6d9798c065b1e2ae65
Prasanth K M <prasanthkm49@gmail.com>
2023-10-05 00:51:14 +0530

    added hotel_search_result_card

---
+++ lib/feature-modules/hotel_booking/ui/components/hote_search_result_card.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class HotelSearchResultCard extends StatefulWidget {
+  const HotelSearchResultCard({super.key});
+
+  @override
+  State<HotelSearchResultCard> createState() => _HotelSearchResultCardState();
+}
+
+class _HotelSearchResultCardState extends State<HotelSearchResultCard> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
