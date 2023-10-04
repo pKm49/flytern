20faf5203c6045aebe9211fd054e7b9136cc1755
Prasanth K M <prasanthkm49@gmail.com>
2023-10-04 14:21:24 +0530

    added page for flight-ticket

---
+++ lib/feature-modules/flight_booking/ui/pages/flight_eticket_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class FlightETicketPage extends StatefulWidget {
+  const FlightETicketPage({super.key});
+
+  @override
+  State<FlightETicketPage> createState() => _FlightETicketPageState();
+}
+
+class _FlightETicketPageState extends State<FlightETicketPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
