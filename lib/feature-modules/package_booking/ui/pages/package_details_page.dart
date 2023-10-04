264af95bba4f9b35db3f16ad166e4e71e782fdf3
Prasanth K M <prasanthkm49@gmail.com>
2023-10-05 03:35:33 +0530

    added package_details_page

---
+++ lib/feature-modules/package_booking/ui/pages/package_details_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class PackageDetailsPage extends StatefulWidget {
+  const PackageDetailsPage({super.key});
+
+  @override
+  State<PackageDetailsPage> createState() => _PackageDetailsPageState();
+}
+
+class _PackageDetailsPageState extends State<PackageDetailsPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
