eae569c301b1776dc256f670978c01576b5a9b15
Prasanth K M <prasanthkm49@gmail.com>
2023-10-04 14:14:21 +0530

    added page for selecting addon services for flightbooking

---
+++ lib/feature-modules/flight_booking/ui/pages/flight_addon_services_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class FlightAddonServicesPage extends StatefulWidget {
+  const FlightAddonServicesPage({super.key});
+
+  @override
+  State<FlightAddonServicesPage> createState() => _FlightAddonServicesPageState();
+}
+
+class _FlightAddonServicesPageState extends State<FlightAddonServicesPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
