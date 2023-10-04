6552ad90f42875a547ceae37f5eac5052dc4244a
Prasanth K M <prasanthkm49@gmail.com>
2023-10-04 14:22:00 +0530

    added page for userdata submission in flightbooking

---
+++ lib/feature-modules/flight_booking/ui/pages/flight_userdetails_submission_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class FlightUserDetailsSubmissionPage extends StatefulWidget {
+  const FlightUserDetailsSubmissionPage({super.key});
+
+  @override
+  State<FlightUserDetailsSubmissionPage> createState() => _FlightUserDetailsSubmissionPageState();
+}
+
+class _FlightUserDetailsSubmissionPageState extends State<FlightUserDetailsSubmissionPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
