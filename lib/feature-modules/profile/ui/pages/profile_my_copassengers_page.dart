9feec35ce07e3e0cba7012ff98dd7ed99b4bbe35
Prasanth K M <prasanthkm49@gmail.com>
2023-10-05 05:03:39 +0530

    added my co passengers page

---
+++ lib/feature-modules/profile/ui/pages/profile_my_copassengers_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ProfileMyCoPassengersPage extends StatefulWidget {
+  const ProfileMyCoPassengersPage({super.key});
+
+  @override
+  State<ProfileMyCoPassengersPage> createState() => _ProfileMyCoPassengersPageState();
+}
+
+class _ProfileMyCoPassengersPageState extends State<ProfileMyCoPassengersPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
