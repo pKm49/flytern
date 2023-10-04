70e972f74177df9806f727875c1287479452c265
Prasanth K M <prasanthkm49@gmail.com>
2023-10-05 04:22:30 +0530

    added navigation to activites details

--- lib/feature-modules/activity_booking/ui/components/activity_list_card.dart
+++ lib/feature-modules/activity_booking/ui/components/activity_list_card.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flytern/shared/data/constants/app_specific/app_route_names.dart';
 import 'package:flytern/shared/data/constants/ui_constants/style_params.dart';
 import 'package:flytern/shared/data/constants/ui_constants/widget_styles.dart';
 import 'package:flytern/shared/services/utility-services/widget_generator.dart';
 import 'package:flytern/shared/services/utility-services/widget_properties_generator.dart';
+import 'package:get/get.dart';
 import 'package:ionicons/ionicons.dart';
 
 class ActivityListCard extends StatelessWidget {
@@ -30,67 +32,72 @@ class ActivityListCard extends StatelessWidget {
     double screenwidth = MediaQuery.of(context).size.width;
     double screenheight = MediaQuery.of(context).size.height;
 
-    return Container(
-      decoration:  BoxDecoration(
-        color: flyternBackgroundWhite,
-      ),
-      padding: flyternMediumPaddingAll,
-      child: Row(
-        children: [
-          Container(
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(flyternBorderRadiusExtraSmall),
-              ),
-              clipBehavior: Clip.hardEdge,
-              child: Image.asset(imageUrl,width: screenwidth*.25, height:  screenwidth*.25)),
-          addHorizontalSpace(flyternSpaceMedium),
-          Expanded(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(title,style: getBodyMediumStyle(context).copyWith(fontWeight: flyternFontWeightBold),),
+    return InkWell(
+      onTap: (){
+        Get.toNamed(Approute_activitiesDetails);
+      },
+      child: Container(
+        decoration:  BoxDecoration(
+          color: flyternBackgroundWhite,
+        ),
+        padding: flyternMediumPaddingAll,
+        child: Row(
+          children: [
+            Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(flyternBorderRadiusExtraSmall),
+                ),
+                clipBehavior: Clip.hardEdge,
+                child: Image.asset(imageUrl,width: screenwidth*.25, height:  screenwidth*.25)),
+            addHorizontalSpace(flyternSpaceMedium),
+            Expanded(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(title,style: getBodyMediumStyle(context).copyWith(fontWeight: flyternFontWeightBold),),
 
-              addVerticalSpace(flyternSpaceSmall),
-              Row(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Text('AED 15,000',style: getBodyMediumStyle(context).copyWith(fontWeight: flyternFontWeightBold, color: flyternSecondaryColor),),
+                addVerticalSpace(flyternSpaceSmall),
+                Row(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text('AED 15,000',style: getBodyMediumStyle(context).copyWith(fontWeight: flyternFontWeightBold, color: flyternSecondaryColor),),
 
 
-                ],
-              ),
+                  ],
+                ),
 
-              addVerticalSpace(flyternSpaceSmall),
-              Row(
-                children: [
-                  // Expanded(child:  Row(
-                  //   mainAxisAlignment: MainAxisAlignment.start,
-                  //   children: [
-                  //     Icon(Ionicons.ticket_outline,color: flyternGrey40),
-                  //     addHorizontalSpace(flyternSpaceSmall),
-                  //     Text(flightName,
-                  //         style:  getBodyMediumStyle(context).copyWith(color: flyternGrey40)),
-                  //   ],
-                  // ),),
-                  //
-                  // addHorizontalSpace(flyternSpaceSmall),
-                  Expanded(
-                      flex: 1,
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.start,
-                        children: [
-                      Icon(Ionicons.location_outline,color: flyternGrey40),
-                      addHorizontalSpace(flyternSpaceSmall),
-                      Text(sponsoredBy,style: getBodyMediumStyle(context).copyWith(color: flyternGrey40),),
-                    ],
-                  )),
-                ],
-              )
-            ],
-          ))
+                addVerticalSpace(flyternSpaceSmall),
+                Row(
+                  children: [
+                    // Expanded(child:  Row(
+                    //   mainAxisAlignment: MainAxisAlignment.start,
+                    //   children: [
+                    //     Icon(Ionicons.ticket_outline,color: flyternGrey40),
+                    //     addHorizontalSpace(flyternSpaceSmall),
+                    //     Text(flightName,
+                    //         style:  getBodyMediumStyle(context).copyWith(color: flyternGrey40)),
+                    //   ],
+                    // ),),
+                    //
+                    // addHorizontalSpace(flyternSpaceSmall),
+                    Expanded(
+                        flex: 1,
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          children: [
+                        Icon(Ionicons.location_outline,color: flyternGrey40),
+                        addHorizontalSpace(flyternSpaceSmall),
+                        Text(sponsoredBy,style: getBodyMediumStyle(context).copyWith(color: flyternGrey40),),
+                      ],
+                    )),
+                  ],
+                )
+              ],
+            ))
 
-        ],
+          ],
+        ),
       ),
     );
   }
