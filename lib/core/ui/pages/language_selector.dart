384ea64d92512c35910ea4bab2c15b5e18850b17
Prasanth K M <prasanthkm49@gmail.com>
2023-10-06 18:09:29 +0530

    updated language selection with package

--- lib/core/ui/pages/language_selector.dart
+++ lib/core/ui/pages/language_selector.dart
@@ -1,3 +1,4 @@
+import 'package:country_code_picker/country_code_picker.dart';
 import 'package:flutter/material.dart';
 import 'package:flytern/core/controllers/core_controller.dart';
 import 'package:flytern/core/data/constants/business-specific/valid_languages.dart';
@@ -61,113 +62,125 @@ class _CoreLanguageSelectorState extends State<CoreLanguageSelector> {
     double screenwidth = MediaQuery.of(context).size.width;
     double screenheight = MediaQuery.of(context).size.height;
 
-    return  Container(
+    return  Scaffold(
+      body: Container(
 
-      height: screenheight,
-      width: screenwidth,
-      padding: EdgeInsets.only(top: flyternSpaceLarge*2),
-      child:Stack(
-        children: [
-          Container(
+        height: screenheight,
+        width: screenwidth,
+        child:Stack(
+          children: [
+            Container(
+                width: screenwidth,
+                padding: EdgeInsets.only(bottom: flyternSpaceLarge*2),
+                height: screenheight ,
+                child: FutureBuilder(
+                  future: _initializeVideoPlayerFuture,
+                  builder: (context, snapshot) {
+                    if (snapshot.connectionState == ConnectionState.done) {
+                      // If the VideoPlayerController has finished initialization, use
+                      // the data it provides to limit the aspect ratio of the video.
+                      return AspectRatio(
+                        aspectRatio: 9/16,
+                        // Use the VideoPlayer widget to display the video.
+                        child: VideoPlayer(_controller),
+                      );
+                    } else {
+                      // If the VideoPlayerController is still initializing, show a
+                      // loading spinner.
+                      return const Center(
+                        child: CircularProgressIndicator(),
+                      );
+                    }
+                  },
+                )
+            ),
+            Container(
+              height: screenheight,
               width: screenwidth,
-              padding: EdgeInsets.only(bottom: flyternSpaceLarge*2),
-              height: screenheight ,
-              child: FutureBuilder(
-                future: _initializeVideoPlayerFuture,
-                builder: (context, snapshot) {
-                  if (snapshot.connectionState == ConnectionState.done) {
-                    // If the VideoPlayerController has finished initialization, use
-                    // the data it provides to limit the aspect ratio of the video.
-                    return AspectRatio(
-                      aspectRatio: 9/16,
-                      // Use the VideoPlayer widget to display the video.
-                      child: VideoPlayer(_controller),
-                    );
-                  } else {
-                    // If the VideoPlayerController is still initializing, show a
-                    // loading spinner.
-                    return const Center(
-                      child: CircularProgressIndicator(),
-                    );
-                  }
-                },
-              )
-          ),
-          Container(
-            height: screenheight,
-            width: screenwidth,
-            child: Column(
-              children: [
-                Expanded(
-                    child: Container(
-                      alignment: Alignment.bottomCenter,
-                      padding: flyternLargePaddingAll*2.5,
-                      width: screenwidth,
-                      child:  Wrap(
-                        children: [
-                          SizedBox(
-                            width: double.infinity,
-                            child: ElevatedButton(
-                              child:   Text("Continue In English" ),
-                              onPressed: () async {
-                                await coreController.changeLanguage(Lang_English);
-                                Get.toNamed(Approute_authSelector);
-
-                              },
-                              style: ButtonStyle(
-                                  padding: MaterialStateProperty.all<EdgeInsetsGeometry>(
-                                      EdgeInsets.symmetric(
-                                          horizontal: flyternSpaceLarge, vertical: flyternSpaceMedium*1.2)),
-                                  backgroundColor: MaterialStateProperty.all<Color>(flyternSecondaryColor)
-                              ),),
-                          ),
-                          SizedBox(height: flyternSpaceLarge,width: 20,),
-                          SizedBox(
-                            width: double.infinity,
-                            child: ElevatedButton(
+              child: Column(
+                children: [
+                  Expanded(
+                      child: Container(
+                        alignment: Alignment.bottomCenter,
+                        padding: flyternLargePaddingAll*2.5,
+                        width: screenwidth,
+                        child:  Wrap(
+                          children: [
+                            SizedBox(
+                              width: double.infinity,
+                              child: ElevatedButton(
+                                child:   Text("Continue In English" ),
                                 onPressed: () async {
-                                  print("ar pressed");
-                                  await coreController.changeLanguage(Lang_Arabic);
+                                  await coreController.changeLanguage(Lang_English);
                                   Get.toNamed(Approute_authSelector);
+
                                 },
                                 style: ButtonStyle(
-                                  padding: MaterialStateProperty.all<EdgeInsetsGeometry>(
-                                      EdgeInsets.symmetric(
-                                          horizontal: flyternSpaceLarge, vertical: flyternSpaceMedium*.9)),
-                                ),
-                                child:Text("اللغه العربيه"  )),
-                          ),
-                        ],
-                      ),
-                    )),
-                Container(
-                  width: screenwidth,
-                  height: (screenwidth*0.6 ).toDouble(),
-                  child: CustomPaint(
-                    size: Size(screenwidth, (screenwidth*0.6 ).toDouble()), //You can Replace [WIDTH] with your desired width for Custom Paint and height will be calculated automatically
-                    painter: LanguageSelectorCurveClipper(),
-                    child: Container(
-                      padding: flyternLargePaddingVertical*2.5,
-                      alignment: Alignment.bottomCenter,
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Text("🇰🇼",style: getHeadlineMediumStyle(context),),
-                          addHorizontalSpace(flyternSpaceSmall),
-                          Text("Kuwait",style: getHeadlineMediumStyle(context).copyWith(color: flyternPrimaryColor,fontWeight: flyternFontWeightRegular)),
-                          addHorizontalSpace(flyternSpaceSmall),
-                          Icon(Ionicons.chevron_down,color: flyternGrey60)
-                        ],
+                                    padding: MaterialStateProperty.all<EdgeInsetsGeometry>(
+                                        EdgeInsets.symmetric(
+                                            horizontal: flyternSpaceLarge, vertical: flyternSpaceMedium*1.2)),
+                                    backgroundColor: MaterialStateProperty.all<Color>(flyternSecondaryColor)
+                                ),),
+                            ),
+                            SizedBox(height: flyternSpaceLarge,width: 20,),
+                            SizedBox(
+                              width: double.infinity,
+                              child: ElevatedButton(
+                                  onPressed: () async {
+                                    print("ar pressed");
+                                    await coreController.changeLanguage(Lang_Arabic);
+                                    Get.toNamed(Approute_authSelector);
+                                  },
+                                  style: ButtonStyle(
+                                    padding: MaterialStateProperty.all<EdgeInsetsGeometry>(
+                                        EdgeInsets.symmetric(
+                                            horizontal: flyternSpaceLarge, vertical: flyternSpaceMedium*.9)),
+                                  ),
+                                  child:Text("اللغه العربيه"  )),
+                            ),
+                          ],
+                        ),
+                      )),
+                  Container(
+                    width: screenwidth,
+                    height: (screenwidth*0.6 ).toDouble(),
+                    child: CustomPaint(
+                      size: Size(screenwidth, (screenwidth*0.6 ).toDouble()), //You can Replace [WIDTH] with your desired width for Custom Paint and height will be calculated automatically
+                      painter: LanguageSelectorCurveClipper(),
+                      child: Container(
+                        padding: flyternLargePaddingVertical,
+                        alignment: Alignment.bottomCenter,
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            addHorizontalSpace(flyternSpaceLarge),
+                            CountryCodePicker(
+                              padding: EdgeInsets.zero,
+                              showDropDownButton: true,
+                              onChanged: print,
+                              // Initial selection and favorite can be one of code ('IT') OR dial_code('+39')
+                              initialSelection: 'KW',
+                              // optional. Shows only country name and flag
+                              showCountryOnly: false,
+                              // optional. Shows only country name and flag when popup is closed.
+                              showOnlyCountryWhenClosed: true,
+                              // optional. aligns the flag and the Text left
+                              alignLeft: false,
+                            ),
+                          ],
+                        ),
                       ),
                     ),
                   ),
-                ),
 
-              ],
+                ],
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
+
+
 }
