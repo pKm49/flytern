2f39c747aef484f1a5bcd7eccd3621ed9e84c2db
Prasanth K M <prasanthkm49@gmail.com>
2023-10-09 22:27:26 +0530

    updated getGuestToken with AuthToken model

--- lib/core/services/http-services/core_http.dart
+++ lib/core/services/http-services/core_http.dart
@@ -1,6 +1,7 @@
 import 'dart:developer';
 
 import 'package:flytern/core/data/constants/app-spectific/http_request_endpoints.dart';
+import 'package:flytern/core/data/models/app-specific/auth_token.dart';
 import 'package:flytern/shared/data/models/app_specific/flytern_http_response.dart';
 import 'package:flytern/shared/services/http-services/http_request_handler.dart';
 
@@ -12,10 +13,12 @@ class CoreHttpServices{
     FlyternHttpResponse response = await getRequest(CoreHttpRequestEndpointGetGuestToken,null);
     if(response.success){
       if(response.data != null){
-          return response.data;
+        AuthToken authToken = mapAuthToken(response.data);
+        return authToken;
       }
     }
     return null;
+
   }
 
   Future<FlyternHttpResponse> updateFirebaseToken(flavor,userId,String firebaseToken) async {
@@ -25,7 +28,5 @@ class CoreHttpServices{
 
     return response;
   }
-
-
 
 }
