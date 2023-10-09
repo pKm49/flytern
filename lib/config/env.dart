83163b44eed6db44e4c6eee7e435a92c707c6235
Prasanth K M <prasanthkm49@gmail.com>
2023-10-09 20:59:07 +0530

    removed apitoken, added basictoken

--- lib/config/env.dart
+++ lib/config/env.dart
@@ -1,5 +1,5 @@
 //const apiEndPoint = "api.samrambhakar.com";
-const apiEndPoint = "http://192.168.1.9:3002";
+const apiEndPoint = "https://flytern.com/coreapi";
 
 const firebaseConfig= {
   "apiKey": "AIzaSyBCHln2k-jA0_grdjEakhLjo6IubzJTf7s",
@@ -12,4 +12,4 @@ const firebaseConfig= {
   "measurementId": "G-EQDDGR67X4"
 };
 
-const apiKey =  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhcGlLZXkiOiJlYzBkOTFhZGZjNjk0Zjc2NjhlYWQxNjhhMTc4ZmU4MDphNTAzZDYwM2Y0ZjM1NmM0M2ZlMTNkYTU2ZTMyYWE0MDU3OThlYjhlMjljMDRmM2U2ODYyNTBjNjUwZDgyYTM1NDczYjM4YjFlOGUxMGRlYmU5MDUzNzJlNDA2ZWMzYmQiLCJpYXQiOjE2MDIwODk4NDV9.uPCxq0qMyGAZ83cFDAyX0tw-CvgdWcGvqkSFq3H49Vk";
+const basicToken = "YWRtaW46OWIyNjg0Zg==";
