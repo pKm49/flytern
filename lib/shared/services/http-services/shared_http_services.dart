ac5266ca8ee121494aaed5631f728cfcb52ec4bc
Prasanth K M <prasanthkm49@gmail.com>
2023-10-10 23:51:19 +0530

    added method for SetDeviceInfo request

--- lib/shared/services/http-services/shared_http_services.dart
+++ lib/shared/services/http-services/shared_http_services.dart
@@ -1,5 +1,6 @@
 import 'package:flytern/shared/data/constants/app_specific/shared_http_request_endpoints.dart';
 import 'package:flytern/shared/data/models/app_specific/flytern_http_response.dart';
+import 'package:flytern/shared/data/models/app_specific/set_device_info_request_body.dart';
 import 'package:flytern/shared/data/models/business_models/language.dart';
 import 'package:flytern/shared/data/models/business_models/support_info.dart';
 import 'package:flytern/shared/services/http-services/http_request_handler.dart';
@@ -19,5 +20,19 @@ class SharedHttpService {
 
     return response;
   }
+
+  setDeviceInfo(SetDeviceInfoRequestBody setDeviceInfoRequestBody ) async {
+
+    FlyternHttpResponse response = await postRequest(
+        SharedHttpRequestEndpointSetDeviceInfo,setDeviceInfoRequestBody.toJson());
+
+    print("setDeviceInfo response");
+    print(response.success);
+    print(response.data);
+    print(response.errors);
+    print(response.message);
+
+  }
+
 
 }
