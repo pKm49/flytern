43373e8fed11d69887688f83d88e6f1591e25e41
Prasanth K M <prasanthkm49@gmail.com>
2023-10-10 23:51:31 +0530

    added method for SetDeviceInfo request

--- lib/shared/controllers/shared_controller.dart
+++ lib/shared/controllers/shared_controller.dart
@@ -3,6 +3,7 @@ import 'dart:ui';
 import 'package:flytern/core/data/constants/business-specific/valid_languages.dart';
 import 'package:flytern/shared/data/constants/business_constants/available_countries.dart';
 import 'package:flytern/shared/data/constants/business_constants/available_languages.dart';
+import 'package:flytern/shared/data/models/app_specific/set_device_info_request_body.dart';
 import 'package:flytern/shared/data/models/business_models/country.dart';
 import 'package:flytern/shared/data/models/business_models/language.dart';
 import 'package:flytern/shared/data/models/business_models/support_info.dart';
@@ -12,6 +13,7 @@ import 'package:shared_preferences/shared_preferences.dart';
 
 class SharedController extends GetxController {
 
+  var sharedHttpService = SharedHttpService();
   var selectedLanguage = Language(name: "English", code: "en").obs;
   var selectedCountry = Country(
       countryName: "India",
@@ -30,12 +32,9 @@ class SharedController extends GetxController {
 
     languages.value = availableLanguages;
     countries.value = availableCountries;
-
-
   }
 
   changeLanguage(Language language) async {
-
     selectedLanguage.value = language;
     Get.updateLocale(Locale(language.code));
     SharedPreferences sharedPreferences = await SharedPreferences.getInstance();
@@ -43,25 +42,38 @@ class SharedController extends GetxController {
   }
 
   changeCountry(Country country) async {
-
     selectedCountry.value = country;
     SharedPreferences sharedPreferences = await SharedPreferences.getInstance();
     await sharedPreferences.setString('selectedCountry', country.countryCode);
-
   }
 
   Future<void> getInitialInfo() async {
-    print("shared getInitialInfo");
-
-    var sharedHttpService = SharedHttpService();
-
     SupportInfo supportInfo = await sharedHttpService.getInitialSupportInfo();
 
     languages.value = supportInfo.languages;
     countries.value = supportInfo.countries;
-
-    print("languages.value");
-    print(languages.length);
-    print(countries.length);
   }
+
+  Future<void> setDeviceLanguageAndCountry() async {
+
+    String firebaseToken = await getFirebaseMessagingToken();
+
+    SetDeviceInfoRequestBody setDeviceInfoRequestBody = SetDeviceInfoRequestBody(
+        language: selectedLanguage.value.code,
+        countryCode: selectedCountry.value.countryCode,
+        notificationEnabled: true,
+        notificationToken: firebaseToken
+    );
+
+    await sharedHttpService.setDeviceInfo(setDeviceInfoRequestBody);
+  }
+
+
+  Future<String> getFirebaseMessagingToken() async {
+    // String firebaseMessagingToken = await FirebaseMessaging.instance.getToken()??"";
+    // print('fcm : ' + firebaseMessagingToken);
+    // return firebaseMessagingToken;
+    return "123456896";
+  }
+
 }
