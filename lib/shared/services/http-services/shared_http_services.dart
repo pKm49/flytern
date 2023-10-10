ea726518edff74660c167423bf86044bd9e6a2e4
Prasanth K M <prasanthkm49@gmail.com>
2023-10-10 16:55:37 +0530

    updated getInitialSupportInfo function to GET and removed unwanted params

--- lib/shared/services/http-services/shared_http_services.dart
+++ lib/shared/services/http-services/shared_http_services.dart
@@ -1,15 +1,21 @@
 import 'package:flytern/shared/data/constants/app_specific/shared_http_request_endpoints.dart';
 import 'package:flytern/shared/data/models/app_specific/flytern_http_response.dart';
+import 'package:flytern/shared/data/models/business_models/language.dart';
+import 'package:flytern/shared/data/models/business_models/support_info.dart';
 import 'package:flytern/shared/services/http-services/http_request_handler.dart';
 
 class SharedHttpService {
 
-  getInitialSupportInfo(bool notificationEnabled, String notificationToken) async {
-    FlyternHttpResponse response = await postRequest(
-        SharedHttpRequestEndpointGetInitalInfo, {
-      notificationEnabled: notificationEnabled,
-      notificationToken: notificationToken
-    });
+  getInitialSupportInfo( ) async {
+    FlyternHttpResponse response = await getRequest(
+        SharedHttpRequestEndpointGetInitalInfo, null);
+
+    if(response.success){
+      if(response.data != null){
+        SupportInfo supportInfo = mapSupportInfo(response.data);
+        return supportInfo;
+      }
+    }
 
     return response;
   }
