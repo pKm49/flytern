331a3efae16e883b32d949c9bccfb90a91447675
Prasanth K M <prasanthkm49@gmail.com>
2023-10-10 20:48:16 +0530

    removed arabicName

--- lib/shared/data/models/business_models/general_item.dart
+++ lib/shared/data/models/business_models/general_item.dart
@@ -2,18 +2,15 @@ class GeneralItem {
 
   final int id;
   final String name;
-  final String arabicName;
 
   GeneralItem({
     required this.id,
     required this.name,
-    required this.arabicName,
   });
 
   Map toJson() => {
     'id': id,
     'name': name,
-    'arabicName': arabicName,
   };
 
 }
@@ -22,6 +19,5 @@ GeneralItem mapGeneralItem(dynamic payload){
   return GeneralItem(
     id :payload["id"]??-1,
     name :payload["name"]??"",
-    arabicName :payload["arabic_name"] != null?payload["arabic_name"].toString():"",
   );
 }
