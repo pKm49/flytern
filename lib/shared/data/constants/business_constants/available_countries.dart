a3dd1caa9a7b0d335c1074dcee8abe9c194d81d5
Prasanth K M <prasanthkm49@gmail.com>
2023-10-10 20:29:56 +0530

    added available countries list as fallback list

---
+++ lib/shared/data/constants/business_constants/available_countries.dart
@@ -0,0 +1,19 @@
+import 'package:flytern/shared/data/models/business_models/country.dart';
+
+final List<Country> availableCountries = [
+  Country(
+      countryName: "Kuwait",
+      countryCode: "KWT",
+      countryISOCode: "KW",
+      countryName_Ar: "الكويت",
+      flag: "https://flagcdn.com/48x36/kw.png",
+      code: "+965"),
+
+  Country(
+      countryName: "India",
+      countryCode: "IND",
+      countryISOCode: "IN",
+      countryName_Ar: "الهند",
+      flag: "https://flagcdn.com/48x36/in.png",
+      code: "+91"),
+];
