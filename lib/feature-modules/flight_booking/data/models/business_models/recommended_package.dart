e4045ba8455e7ddb3aacddfa29b8ec90a0678929
Prasanth K M <prasanthkm49@gmail.com>
2023-10-11 18:09:46 +0530

    added data model for recommended package data

---
+++ lib/feature-modules/flight_booking/data/models/business_models/recommended_package.dart
@@ -0,0 +1,43 @@
+class RecommendedPackage {
+
+  final int refId;
+  final String name;
+  final String shortDesc;
+  final String destinations;
+  final String url;
+  final String urlType;
+  final String ratings;
+
+  RecommendedPackage({
+    required this.refId,
+    required this.name,
+    required this.shortDesc,
+    required this.destinations,
+    required this.url,
+    required this.urlType,
+    required this.ratings,
+  });
+
+  Map toJson() => {
+    'refId': refId,
+    'name': name,
+    'shortDesc': shortDesc,
+    'destinations': destinations,
+    'url': url,
+    'urlType': urlType,
+    'ratings': ratings,
+  };
+
+}
+
+RecommendedPackage mapRecommendedPackage(dynamic payload){
+  return RecommendedPackage(
+    refId :payload["refId"]??-1,
+    name :payload["name"]??"",
+    shortDesc :payload["shortDesc"]??"",
+    destinations :payload["destinations"]??"",
+    url :payload["url"]??"",
+    urlType :payload["urlType"]??"",
+    ratings :payload["ratings"]??"",
+  );
+}
