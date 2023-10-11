49f97c1c26a6400789f5b519324684cd475955c4
Prasanth K M <prasanthkm49@gmail.com>
2023-10-11 18:09:52 +0530

    added data model for travel story data

---
+++ lib/feature-modules/flight_booking/data/models/business_models/travel_story.dart
@@ -0,0 +1,43 @@
+class TravelStory {
+
+  final int refId;
+  final String name;
+  final String shortDesc;
+  final String profileUrl;
+  final String url;
+  final String urlType;
+  final String ratings;
+
+  TravelStory({
+    required this.refId,
+    required this.name,
+    required this.shortDesc,
+    required this.profileUrl,
+    required this.url,
+    required this.urlType,
+    required this.ratings,
+  });
+
+  Map toJson() => {
+    'refId': refId,
+    'name': name,
+    'shortDesc': shortDesc,
+    'profileUrl': profileUrl,
+    'url': url,
+    'urlType': urlType,
+    'ratings': ratings,
+  };
+
+}
+
+TravelStory mapTravelStory(dynamic payload){
+  return TravelStory(
+    refId :payload["refId"]??-1,
+    name :payload["name"]??"",
+    shortDesc :payload["shortDesc"]??"",
+    profileUrl :payload["profileUrl"]??"",
+    url :payload["url"]??"",
+    urlType :payload["urlType"]??"",
+    ratings :payload["ratings"]??"",
+  );
+}
