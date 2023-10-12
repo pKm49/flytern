41676cbf644d457bf849f892510aecb94c6e5d25
Prasanth K M <prasanthkm49@gmail.com>
2023-10-12 14:58:27 +0530

    updated with initial data getter

--- lib/feature-modules/flight_booking/controllers/flight_booking_controller.dart
+++ lib/feature-modules/flight_booking/controllers/flight_booking_controller.dart
@@ -1,3 +1,9 @@
+import 'package:flytern/feature-modules/flight_booking/data/models/business_models/cabin_class.dart';
+import 'package:flytern/feature-modules/flight_booking/data/models/business_models/explore_data.dart';
+import 'package:flytern/feature-modules/flight_booking/data/models/business_models/popular_destination.dart';
+import 'package:flytern/feature-modules/flight_booking/data/models/business_models/recommended_package.dart';
+import 'package:flytern/feature-modules/flight_booking/data/models/business_models/travel_story.dart';
+import 'package:flytern/feature-modules/flight_booking/services/http-services/flight_booking_http_services.dart';
 import 'package:flytern/shared/data/constants/business_constants/available_countries.dart';
 import 'package:flytern/shared/data/constants/business_constants/available_languages.dart';
 import 'package:flytern/shared/data/models/business_models/country.dart';
@@ -8,25 +14,31 @@ import 'package:get/get.dart';
 
 class FlightBookingController extends GetxController {
 
-  var isSetDeviceLanguageAndCountrySubmitting = false.obs;
-  var sharedHttpService = SharedHttpService();
+  var isInitialDataLoading = false.obs;
+  var flightBookingHttpService = FlightBookingHttpService();
 
-  var languages = <Language>[].obs;
-  var countries = <Country>[].obs;
+  var cabinClasses = <CabinClass>[].obs;
+  var recommendedPackages = <RecommendedPackage>[].obs;
+  var popularDestinations = <PopularDestination>[].obs;
+  var travelStories = <TravelStory>[].obs;
 
   @override
   void onInit() {
     super.onInit();
-
-    languages.value = availableLanguages;
-    countries.value = availableCountries;
+    getInitialInfo();
   }
 
   Future<void> getInitialInfo() async {
-    SupportInfo supportInfo = await sharedHttpService.getInitialSupportInfo();
 
-    languages.value = supportInfo.languages;
-    countries.value = supportInfo.countries;
+    ExploreData? exploreData = await flightBookingHttpService.getInitialInfo();
+
+    if(exploreData != null){
+      cabinClasses.value = exploreData.cabinClasses;
+      recommendedPackages.value = exploreData.recommendedPackages;
+      popularDestinations.value = exploreData.popularDestinations;
+      travelStories.value = exploreData.travelStories;
+    }
+
   }
 
 }
