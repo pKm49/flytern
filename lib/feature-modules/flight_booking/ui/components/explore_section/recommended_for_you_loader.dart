237b7fc5c945c466fcec518028d24392458b446f
Prasanth K M <prasanthkm49@gmail.com>
2023-10-12 15:23:33 +0530

    added widget for recommended for you loader

---
+++ lib/feature-modules/flight_booking/ui/components/explore_section/recommended_for_you_loader.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:flytern/feature-modules/flight_booking/ui/components/recommended_item_card.dart';
+import 'package:flytern/shared/data/constants/ui_constants/asset_urls.dart';
+import 'package:flytern/shared/data/constants/ui_constants/style_params.dart';
+import 'package:flytern/shared/data/constants/ui_constants/widget_styles.dart';
+import 'package:flytern/shared/services/utility-services/widget_generator.dart';
+import 'package:shimmer/shimmer.dart';
+
+class RecommendedForYouLoader extends StatelessWidget {
+  const RecommendedForYouLoader({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+
+    double screenwidth = MediaQuery.of(context).size.width;
+    double screenheight = MediaQuery.of(context).size.height;
+
+    return Container(
+        width: screenwidth,
+        height: screenwidth * .7,
+        padding: flyternMediumPaddingHorizontal,
+        child: ListView(
+          scrollDirection: Axis.horizontal,
+          children: [
+            Shimmer.fromColors(
+              baseColor: flyternBackgroundWhite,
+              highlightColor: flyternGrey20,
+              child: Container(
+                  width: screenwidth * .7,
+                  height: screenwidth * .7,
+                  color:flyternGrey10
+              ),
+            ),
+            addHorizontalSpace(flyternSpaceMedium),
+            FlightRecommendedItemCard(
+                imageUrl: ASSETS_RECOMMENDED_1_SAMPLE,
+                title: "Four Seasons Resort Bora Bora",
+                rating: 4.4
+            ),
+            addHorizontalSpace(flyternSpaceMedium),
+            FlightRecommendedItemCard(
+                imageUrl: ASSETS_RECOMMENDED_1_SAMPLE,
+                title: "Four Seasons Resort Bora Bora",
+                rating: 4.4
+            ),
+          ],
+        ));
+
+  }
+}
