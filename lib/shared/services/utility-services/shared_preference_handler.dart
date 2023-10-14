0242416920df4721ea58dcb71789ecf9f1fb3b50
Prasanth K M <prasanthkm49@gmail.com>
2023-10-14 15:17:11 +0530

    created shared preference handler for common usage

---
+++ lib/shared/services/utility-services/shared_preference_handler.dart
@@ -0,0 +1,10 @@
+import 'package:flytern/shared/data/models/business_models/auth_token.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+saveAuthTokenToSharedPreference(AuthToken authToken) async {
+  final SharedPreferences prefs = await SharedPreferences.getInstance();
+  prefs.setBool("isGuest", authToken.isGuest);
+  prefs.setString("accessToken", authToken.accessToken);
+  prefs.setString("refreshToken", authToken.refreshToken);
+  prefs.setString("expiryOn", authToken.expiryOn.toString());
+}
