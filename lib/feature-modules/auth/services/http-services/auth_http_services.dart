0cb40b6408505b5dde8d00f7e895ad276f5dea2b
Prasanth K M <prasanthkm49@gmail.com>
2023-10-14 15:20:28 +0530

    updated login method with params and return value

--- lib/feature-modules/auth/services/http-services/auth_http_services.dart
+++ lib/feature-modules/auth/services/http-services/auth_http_services.dart
@@ -1,27 +1,29 @@
 import 'package:flytern/feature-modules/auth/data/constants/app_specific/auth_http_request_endpoints.dart';
+import 'package:flytern/feature-modules/auth/data/models/business_models/login_credential.dart';
 import 'package:flytern/feature-modules/flight_booking/data/constants/app_specific/flight_booking_http_request_endpoints.dart';
 import 'package:flytern/feature-modules/flight_booking/data/models/business_models/explore_data.dart';
 import 'package:flytern/shared/data/constants/app_specific/shared_http_request_endpoints.dart';
 import 'package:flytern/shared/data/models/app_specific/flytern_http_response.dart';
 import 'package:flytern/shared/data/models/app_specific/set_device_info_request_body.dart';
+import 'package:flytern/shared/data/models/business_models/auth_token.dart';
 import 'package:flytern/shared/data/models/business_models/language.dart';
 import 'package:flytern/shared/data/models/business_models/support_info.dart';
 import 'package:flytern/shared/services/http-services/http_request_handler.dart';
 
 class AuthHttpService {
 
-  Future<ExploreData?> login( ) async {
+  Future<AuthToken > login(LoginCredential loginCredential ) async {
     FlyternHttpResponse response = await postRequest(
-        AuthHttpRequestEndpointLogin, null);
+        AuthHttpRequestEndpointLogin, loginCredential.toJson());
 
     if(response.success){
       if(response.data != null){
-        ExploreData exploreData = mapExploreData(response.data);
-        return exploreData;
+        AuthToken authToken = mapAuthToken(response.data, false);
+        return authToken;
       }
     }
 
-    return null;
+    return mapAuthToken({},true);
   }
 
 
