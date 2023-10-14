614f9e0852f3b749e335be93be0a06fe23df26fd
Prasanth K M <prasanthkm49@gmail.com>
2023-10-14 12:12:15 +0530

    added one more loader

--- lib/feature-modules/flight_booking/ui/components/explore_section/popular_destinations_loader.dart
+++ lib/feature-modules/flight_booking/ui/components/explore_section/popular_destinations_loader.dart
@@ -102,7 +102,77 @@ class PopularDestinationsLoader extends StatelessWidget {
             padding: const EdgeInsets.symmetric(horizontal: flyternSpaceMedium),
             child: Divider(),
           ),
+          Container(
+            decoration:  BoxDecoration(
+              color: flyternBackgroundWhite,
+            ),
+            padding: flyternMediumPaddingAll,
+            child: Row(
+              children: [
+                Shimmer.fromColors(
+                  baseColor: flyternBackgroundWhite,
+                  highlightColor: flyternGrey20,
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color:flyternGrey10,
+                      borderRadius:
+                      BorderRadius.circular(flyternBorderRadiusExtraSmall),
+                    ),
+                    width: screenwidth*.25,
+                    height: screenwidth*.25,
+                  ),
+                ),
+                addHorizontalSpace(flyternSpaceMedium),
+                Expanded(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Shimmer.fromColors(
+                          baseColor: flyternBackgroundWhite,
+                          highlightColor: flyternGrey20,
+                          child: Container(
+                            decoration: BoxDecoration(
+                              color:flyternGrey10,
+                              borderRadius:
+                              BorderRadius.circular(flyternBorderRadiusExtraSmall),
+                            ),
+                            height: 30,
+                          ),
+                        ),
+                        addVerticalSpace(flyternSpaceSmall),
+                        Shimmer.fromColors(
+                          baseColor: flyternBackgroundWhite,
+                          highlightColor: flyternGrey20,
+                          child: Container(
+                            decoration: BoxDecoration(
+                              color:flyternGrey10,
+                              borderRadius:
+                              BorderRadius.circular(flyternBorderRadiusExtraSmall),
+                            ),
+                            height: 30,
+                            width: screenwidth*.4,
+                          ),
+                        ),
+                        addVerticalSpace(flyternSpaceSmall),
+                        Shimmer.fromColors(
+                          baseColor: flyternBackgroundWhite,
+                          highlightColor: flyternGrey20,
+                          child: Container(
+                            decoration: BoxDecoration(
+                              color:flyternGrey10,
+                              borderRadius:
+                              BorderRadius.circular(flyternBorderRadiusExtraSmall),
+                            ),
+                            height: 30,
+                            width: screenwidth*.4,
+                          ),
+                        ),
+                      ],
+                    ))
 
+              ],
+            ),
+          ),
         ],
       ),
     );
