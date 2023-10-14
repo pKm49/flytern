3d3cb985f4e42bbdca0f12ab3adac5654ad33506
Prasanth K M <prasanthkm49@gmail.com>
2023-10-14 12:57:54 +0530

    added getPreRegister Support Info getter

--- lib/shared/services/http-services/shared_http_services.dart
+++ lib/shared/services/http-services/shared_http_services.dart
@@ -1,6 +1,8 @@
 import 'package:flytern/shared/data/constants/app_specific/shared_http_request_endpoints.dart';
 import 'package:flytern/shared/data/models/app_specific/flytern_http_response.dart';
 import 'package:flytern/shared/data/models/app_specific/set_device_info_request_body.dart';
+import 'package:flytern/shared/data/models/business_models/business_doc.dart';
+import 'package:flytern/shared/data/models/business_models/general_item.dart';
 import 'package:flytern/shared/data/models/business_models/language.dart';
 import 'package:flytern/shared/data/models/business_models/support_info.dart';
 import 'package:flytern/shared/services/http-services/http_request_handler.dart';
@@ -19,6 +21,20 @@ class SharedHttpService {
     }
 
     return response;
+  }
+
+  getPreRegisterSupportInfo( ) async {
+    FlyternHttpResponse response = await getRequest(
+        SharedHttpRequestEndpointGetPreRegisterInfo, null);
+
+    if(response.success){
+      if(response.data != null){
+        BusinessDoc businessDoc = mapBusinessDoc(response.data);
+        return businessDoc;
+      }
+    }
+
+    return null;
   }
 
   setDeviceInfo(SetDeviceInfoRequestBody setDeviceInfoRequestBody ) async {
