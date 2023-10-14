7943fc3fa6301af05512cbc771a671e5b3f087c7
Prasanth K M <prasanthkm49@gmail.com>
2023-10-14 12:48:21 +0530

    added endpoint for Geting PreRegister Info

--- lib/shared/data/constants/app_specific/shared_http_request_endpoints.dart
+++ lib/shared/data/constants/app_specific/shared_http_request_endpoints.dart
@@ -1,3 +1,4 @@
 const String SharedHttpRequestEndpointGetInitalInfo = "/coreapi/api/Supports/GetInitalInfo";
+const String SharedHttpRequestEndpointGetPreRegisterInfo = "/coreapi/api/Users/PreRegister";
 const String SharedHttpRequestEndpointSetDeviceInfo = "/coreapi/api/Supports/SaveDeviceLang";
 
