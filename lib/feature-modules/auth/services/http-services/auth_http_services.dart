59511c398d35bbc578b52844c6551adf8a740248
Prasanth K M <prasanthkm49@gmail.com>
2023-10-14 12:44:49 +0530

    created auth requests http services

---
+++ lib/feature-modules/auth/services/http-services/auth_http_services.dart
@@ -0,0 +1,28 @@
+import 'package:flytern/feature-modules/auth/data/constants/app_specific/auth_http_request_endpoints.dart';
+import 'package:flytern/feature-modules/flight_booking/data/constants/app_specific/flight_booking_http_request_endpoints.dart';
+import 'package:flytern/feature-modules/flight_booking/data/models/business_models/explore_data.dart';
+import 'package:flytern/shared/data/constants/app_specific/shared_http_request_endpoints.dart';
+import 'package:flytern/shared/data/models/app_specific/flytern_http_response.dart';
+import 'package:flytern/shared/data/models/app_specific/set_device_info_request_body.dart';
+import 'package:flytern/shared/data/models/business_models/language.dart';
+import 'package:flytern/shared/data/models/business_models/support_info.dart';
+import 'package:flytern/shared/services/http-services/http_request_handler.dart';
+
+class AuthHttpService {
+
+  Future<ExploreData?> login( ) async {
+    FlyternHttpResponse response = await postRequest(
+        AuthHttpRequestEndpointLogin, null);
+
+    if(response.success){
+      if(response.data != null){
+        ExploreData exploreData = mapExploreData(response.data);
+        return exploreData;
+      }
+    }
+
+    return null;
+  }
+
+
+}
