03bc214457841d304dd47d38c482a3388b8db9f8
Prasanth K M <prasanthkm49@gmail.com>
2023-10-16 08:31:53 +0530

    copied code from login controller

--- lib/feature-modules/auth/controllers/register_controller.dart
+++ lib/feature-modules/auth/controllers/register_controller.dart
@@ -1,31 +1,49 @@
+import 'package:flutter/material.dart';
+import 'package:flytern/feature-modules/auth/data/models/business_models/login_credential.dart';
 import 'package:flytern/feature-modules/auth/services/http-services/auth_http_services.dart';
-import 'package:flytern/feature-modules/flight_booking/data/models/business_models/cabin_class.dart';
-import 'package:flytern/feature-modules/flight_booking/data/models/business_models/explore_data.dart';
-import 'package:flytern/feature-modules/flight_booking/data/models/business_models/popular_destination.dart';
-import 'package:flytern/feature-modules/flight_booking/data/models/business_models/recommended_package.dart';
-import 'package:flytern/feature-modules/flight_booking/data/models/business_models/travel_story.dart';
-import 'package:flytern/feature-modules/flight_booking/services/http-services/flight_booking_http_services.dart';
-import 'package:flytern/shared/data/constants/business_constants/available_countries.dart';
-import 'package:flytern/shared/data/constants/business_constants/available_languages.dart';
-import 'package:flytern/shared/data/models/business_models/country.dart';
-import 'package:flytern/shared/data/models/business_models/language.dart';
-import 'package:flytern/shared/data/models/business_models/support_info.dart';
-import 'package:flytern/shared/services/http-services/shared_http_services.dart';
+import 'package:flytern/shared/data/constants/app_specific/app_route_names.dart';
+import 'package:flytern/shared/data/constants/ui_constants/style_params.dart';
+import 'package:flytern/shared/data/models/business_models/auth_token.dart';
+import 'package:flytern/shared/services/utility-services/shared_preference_handler.dart';
+import 'package:flytern/shared/services/utility-services/snackbar_shower.dart';
 import 'package:get/get.dart';
 
 class RegisterController extends GetxController {
 
-  var isInitialDataLoading = true.obs;
-  var authHttpService = AuthHttpService();
+  Rx<TextEditingController> emailFieldController = TextEditingController().obs;
+  Rx<TextEditingController> passwordController = TextEditingController().obs;
+  var errorMessage = "".obs;
+  var isSubmitting = false.obs;
 
-  var cabinClasses = <CabinClass>[].obs;
-  var recommendedPackages = <RecommendedPackage>[].obs;
-  var popularDestinations = <PopularDestination>[].obs;
-  var travelStories = <TravelStory>[].obs;
+  var authHttpService = AuthHttpService();
 
   @override
   void onInit() {
     super.onInit();
+  }
+
+  submitLoginForm() async {
+
+    isSubmitting.value = true;
+    try{
+
+      LoginCredential loginCredential = LoginCredential(
+          email: emailFieldController.value.text,
+          password: passwordController.value.text
+      );
+
+      AuthToken authToken  = await authHttpService.login(loginCredential);
+
+      if(authToken.accessToken != ""){
+        saveAuthTokenToSharedPreference(authToken);
+        Get.offAllNamed(Approute_landingpage);
+      }
+      isSubmitting.value = false;
+    }catch (e){
+      showSnackbar( e.toString(),"error");
+      isSubmitting.value = false;
+    }
+
   }
 
 }
