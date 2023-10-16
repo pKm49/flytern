2e5fdf798b931d1af67e17743d912a0214cd964a
Prasanth K M <prasanthkm49@gmail.com>
2023-10-16 14:45:35 +0530

    added logic

--- lib/feature-modules/auth/controllers/reset_password_controller.dart
+++ lib/feature-modules/auth/controllers/reset_password_controller.dart
@@ -1,31 +1,142 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:flytern/feature-modules/auth/data/models/business_models/login_credential.dart';
+import 'package:flytern/feature-modules/auth/data/models/business_models/register_credential.dart';
 import 'package:flytern/feature-modules/auth/services/http-services/auth_http_services.dart';
-import 'package:flytern/feature-modules/flight_booking/data/models/business_models/cabin_class.dart';
-import 'package:flytern/feature-modules/flight_booking/data/models/business_models/explore_data.dart';
-import 'package:flytern/feature-modules/flight_booking/data/models/business_models/popular_destination.dart';
-import 'package:flytern/feature-modules/flight_booking/data/models/business_models/recommended_package.dart';
-import 'package:flytern/feature-modules/flight_booking/data/models/business_models/travel_story.dart';
-import 'package:flytern/feature-modules/flight_booking/services/http-services/flight_booking_http_services.dart';
-import 'package:flytern/shared/data/constants/business_constants/available_countries.dart';
-import 'package:flytern/shared/data/constants/business_constants/available_languages.dart';
+import 'package:flytern/shared/data/constants/app_specific/app_route_names.dart';
+import 'package:flytern/shared/data/constants/ui_constants/style_params.dart';
+import 'package:flytern/shared/data/models/business_models/auth_token.dart';
 import 'package:flytern/shared/data/models/business_models/country.dart';
-import 'package:flytern/shared/data/models/business_models/language.dart';
-import 'package:flytern/shared/data/models/business_models/support_info.dart';
-import 'package:flytern/shared/services/http-services/shared_http_services.dart';
+import 'package:flytern/shared/services/utility-services/shared_preference_handler.dart';
+import 'package:flytern/shared/services/utility-services/snackbar_shower.dart';
 import 'package:get/get.dart';
 
 class ResetPasswordController extends GetxController {
 
-  var isInitialDataLoading = true.obs;
-  var authHttpService = AuthHttpService();
+  Rx<TextEditingController> passwordController = TextEditingController().obs;
+  Rx<TextEditingController> confirmPasswordController = TextEditingController().obs;
+  Rx<TextEditingController> mobileController = TextEditingController().obs;
 
-  var cabinClasses = <CabinClass>[].obs;
-  var recommendedPackages = <RecommendedPackage>[].obs;
-  var popularDestinations = <PopularDestination>[].obs;
-  var travelStories = <TravelStory>[].obs;
+  var selectedCountry = Country(
+      countryName: "India",
+      countryCode: "IND",
+      countryISOCode: "IN",
+      countryName_Ar: "الهند",
+      flag: "https://flagcdn.com/48x36/in.png",
+      code: "+91").obs;
+
+  var userId = "".obs;
+  var otp = "".obs;
+  var profilePicture = "".obs;
+  var errorMessage = "".obs;
+  var isPasswordVisible = false.obs;
+  var isConfirmPasswordVisible = false.obs;
+  var isTermsAndPrivacyAgreed = false.obs;
+  var isSubscribedToEmail = false.obs;
+  var isSubmitting = false.obs;
+
+  var authHttpService = AuthHttpService();
 
   @override
   void onInit() {
     super.onInit();
+  }
+
+  sendOtp() async {
+
+    isSubmitting.value = true;
+    try{
+
+      String userIdFromApi  = await authHttpService.sendOtp(
+      mobileController.value.text
+      ,selectedCountry.value.code);
+
+      if(userIdFromApi != ""){
+        userId.value = userIdFromApi;
+        Get.offAllNamed(Approute_resetPasswordOtp);
+      }else{
+        throw Exception("Something Went wrong, Please Try Again");
+      }
+      isSubmitting.value = false;
+    }catch (e,t){
+      print(t);
+      showSnackbar( e.toString(),"error");
+      isSubmitting.value = false;
+    }
+
+  }
+
+  resendOtp( ) async {
+
+    if(userId.value !=""){
+      isSubmitting.value = true;
+      try{
+
+        await authHttpService.resendOtp(userId.value);
+
+        isSubmitting.value = false;
+      }catch (e,t){
+        print(t);
+        showSnackbar( e.toString(),"error");
+        isSubmitting.value = false;
+      }
+
+    }
+
+  }
+
+  verifyOtp(String otp) async {
+
+    if(userId.value != ""){
+      isSubmitting.value = true;
+      try{
+
+        AuthToken authToken  = await authHttpService.verifyOtp(userId.value,otp);
+
+        if(authToken.accessToken != ""){
+          saveAuthTokenToSharedPreference(authToken);
+          Get.offAllNamed(Approute_resetPasswordNewpassword);
+        }
+        isSubmitting.value = false;
+      }catch (e){
+        showSnackbar( e.toString(),"error");
+        isSubmitting.value = false;
+      }
+    }
+
+  }
+
+  updatePassword(String otp) async {
+
+    if(userId.value != ""){
+      isSubmitting.value = true;
+      try{
+       await authHttpService.updatePassword(passwordController.value.text);
+       Get.offAllNamed(Approute_login);
+       isSubmitting.value = false;
+      }catch (e){
+        showSnackbar( e.toString(),"error");
+        isSubmitting.value = false;
+      }
+    }
+
+  }
+
+  void updateOtp(String otpString) {
+    otp.value = otpString;
+  }
+
+  void updateSubscriptionAgreement(bool bool) {
+    isSubscribedToEmail.value = bool;
+  }
+
+  void updateTermsAndPrivacyAgreement(bool bool) {
+    isTermsAndPrivacyAgreed.value = bool;
+  }
+
+  void updateProfilePicture(String base64encode) {
+    profilePicture.value = base64encode;
   }
 
 }
