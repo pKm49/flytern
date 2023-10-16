806411e08683c7caa7d10c1319be676aec234b0c
Prasanth K M <prasanthkm49@gmail.com>
2023-10-16 10:17:01 +0530

    added method for register

--- lib/feature-modules/auth/services/http-services/auth_http_services.dart
+++ lib/feature-modules/auth/services/http-services/auth_http_services.dart
@@ -1,5 +1,6 @@
 import 'package:flytern/feature-modules/auth/data/constants/app_specific/auth_http_request_endpoints.dart';
 import 'package:flytern/feature-modules/auth/data/models/business_models/login_credential.dart';
+import 'package:flytern/feature-modules/auth/data/models/business_models/register_credential.dart';
 import 'package:flytern/shared/data/models/app_specific/flytern_http_response.dart';
 import 'package:flytern/shared/data/models/business_models/auth_token.dart';
 import 'package:flytern/shared/services/http-services/http_request_handler.dart';
@@ -25,6 +26,28 @@ class AuthHttpService {
    }catch (e){
      rethrow;
    }
+
+  }
+
+  Future<AuthToken > register(RegisterCredential registerCredential ) async {
+    FlyternHttpResponse response = await postRequest(
+        AuthHttpRequestEndpointRegister, registerCredential.toJson());
+
+    print(" response.message ");
+    print(response.message);
+    print(response.errors);
+    print(response.success);
+
+    try{
+      if(response.success && response.data != null){
+        AuthToken authToken = mapAuthToken(response.data, false);
+        return authToken;
+      }else{
+        throw Exception(response.errors[0]);
+      }
+    }catch (e){
+      rethrow;
+    }
 
   }
 
