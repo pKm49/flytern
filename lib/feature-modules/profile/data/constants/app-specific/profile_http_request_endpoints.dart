405826caa31e814381692643c8d232ba5b35ec23
Prasanth K M <prasanthkm49@gmail.com>
2023-10-17 13:26:00 +0530

    added travelstories & copax getter endpoints

--- lib/feature-modules/profile/data/constants/app-specific/profile_http_request_endpoints.dart
+++ lib/feature-modules/profile/data/constants/app-specific/profile_http_request_endpoints.dart
@@ -1,1 +1,3 @@
 const String ProfileHttpRequestEndpointGetUserDetails = "/coreapi/api/Users/GetUserProfile";
+const String ProfileHttpRequestEndpointGetUserTravelStories = "/coreapi/api/TravelStories/GetTravelStories";
+const String ProfileHttpRequestEndpointGetUserCoPaxs = "/coreapi/api/CoPaxs/GetUserCoPaxs";
