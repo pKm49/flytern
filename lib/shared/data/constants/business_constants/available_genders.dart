cbd258ef78428d04b7abd8cd1cdd25f97fb7919a
Prasanth K M <prasanthkm49@gmail.com>
2023-10-17 18:56:32 +0530

    added default list of genders

---
+++ lib/shared/data/constants/business_constants/available_genders.dart
@@ -0,0 +1,6 @@
+import 'package:flytern/shared/data/models/business_models/gender.dart';
+
+final List<Gender> availableGenders = [
+  Gender(name: "Male", code: "Male", isDefault: true),
+  Gender(name: "Female", code: "Female", isDefault: false)
+];
