27d1b12c449efb9d0b5ca32c042cf9a8fd3f6956
Prasanth K M <prasanthkm49@gmail.com>
2023-10-17 16:32:32 +0530

    added photoselector

--- lib/feature-modules/profile/ui/pages/profile_new_travel_story_page.dart
+++ lib/feature-modules/profile/ui/pages/profile_new_travel_story_page.dart
@@ -1,10 +1,14 @@
+import 'dart:io';
+
 import 'package:dotted_border/dotted_border.dart';
 import 'package:flutter/material.dart';
+import 'package:flytern/feature-modules/profile/controllers/travel_story_controller.dart';
 import 'package:flytern/shared/data/constants/ui_constants/asset_urls.dart';
 import 'package:flytern/shared/data/constants/ui_constants/style_params.dart';
 import 'package:flytern/shared/data/constants/ui_constants/widget_styles.dart';
 import 'package:flytern/shared/services/utility-services/widget_generator.dart';
 import 'package:flytern/shared/services/utility-services/widget_properties_generator.dart';
+import 'package:flytern/shared/ui/components/photo_selector.dart';
 import 'package:get/get.dart';
 import 'package:ionicons/ionicons.dart';
 
@@ -18,6 +22,8 @@ class ProfileNewTravelStoryPage extends StatefulWidget {
 class _ProfileNewTravelStoryPageState extends State<ProfileNewTravelStoryPage> {
 
   double rating = 3.0;
+  final travelStoryController = Get.find<TravelStoryController>();
+  late File mediaFile;
 
   @override
   Widget build(BuildContext context) {
@@ -37,29 +43,34 @@ class _ProfileNewTravelStoryPageState extends State<ProfileNewTravelStoryPage> {
           children: [
             Padding(
               padding: flyternLargePaddingAll,
-              child: DottedBorder(
-                borderType: BorderType.RRect,
-                color: flyternSecondaryColor,
-                dashPattern: [6, 6, 6, 6],
-                radius:
-                Radius.circular(flyternBorderRadiusSmall),
-                strokeWidth: 1,
-                child: ClipRRect(
-                  borderRadius: BorderRadius.all(Radius.circular(
-                      flyternBorderRadiusSmall)),
-                  child: Container(
-                    width: screenwidth-(flyternSpaceLarge*2),
-                    padding: flyternLargePaddingAll,
-                    color: flyternSecondaryColorBg,
-                    height: 200,
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      children: [
-                        Image.asset(ASSETS_FILE_UPLOAD_ICON,width: screenwidth*.2),
-                        addVerticalSpace(flyternSpaceLarge),
-                        Text("upload_image_video".tr,style: getBodyMediumStyle(context))
-                      ],
+              child: InkWell(
+                onTap:(){
+                  openSourceSelector(context);
+                },
+                child: DottedBorder(
+                  borderType: BorderType.RRect,
+                  color: flyternSecondaryColor,
+                  dashPattern: [6, 6, 6, 6],
+                  radius:
+                  Radius.circular(flyternBorderRadiusSmall),
+                  strokeWidth: 1,
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.all(Radius.circular(
+                        flyternBorderRadiusSmall)),
+                    child: Container(
+                      width: screenwidth-(flyternSpaceLarge*2),
+                      padding: flyternLargePaddingAll,
+                      color: flyternSecondaryColorBg,
+                      height: 200,
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          Image.asset(ASSETS_FILE_UPLOAD_ICON,width: screenwidth*.2),
+                          addVerticalSpace(flyternSpaceLarge),
+                          Text("upload_image_video".tr,style: getBodyMediumStyle(context))
+                        ],
+                      ),
                     ),
                   ),
                 ),
@@ -134,5 +145,32 @@ class _ProfileNewTravelStoryPageState extends State<ProfileNewTravelStoryPage> {
           ),
         )
     );
+  }
+
+
+  void openSourceSelector(context) {
+    showModalBottomSheet(
+        useSafeArea: false,
+        shape: const RoundedRectangleBorder(
+          borderRadius: BorderRadius.only(
+              topLeft: Radius.circular(flyternBorderRadiusSmall),
+              topRight: Radius.circular(flyternBorderRadiusSmall)),
+        ),
+        isScrollControlled: true,
+        context: context,
+        builder: (context) {
+          return PhotoSelector(
+            photoSelected: (File? selectedMediaFile) {
+              if (selectedMediaFile != null) {
+                mediaFile = selectedMediaFile;
+              }
+              setState(() {});
+            }, isVideosAllowed: true
+
+          );
+        });
+
+
+
   }
 }
