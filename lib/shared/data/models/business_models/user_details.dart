1079e1d9c6a7a22eaa932ee28191cbf02b68b527
Prasanth K M <prasanthkm49@gmail.com>
2023-10-17 12:18:00 +0530

    added userdetails model

---
+++ lib/shared/data/models/business_models/user_details.dart
@@ -0,0 +1,67 @@
+
+import 'package:flytern/shared/data/constants/app_specific/default_values.dart';
+
+class UserDetails {
+
+
+  final String  userName;
+  final String  email;
+  final String phoneNumber;
+  final String gender;
+  final String firstName;
+  final String lastName;
+  final String phoneCountryCode;
+  final String imgUrl;
+  final String passportNumber;
+  final DateTime passportExpiry;
+  final DateTime dateOfBirth;
+  final DateTime passportIssuerCountryCode;
+  final DateTime passportIssuerCountryName;
+  final DateTime nationalityCode;
+  final DateTime nationalityName;
+
+
+  UserDetails({
+    required this.gender, required this.firstName, required this.lastName,
+    required this.phoneCountryCode, required this.imgUrl, required this.passportNumber,
+    required this.dateOfBirth, required this.passportIssuerCountryCode, required this.passportIssuerCountryName,
+    required this.nationalityCode, required this.nationalityName,
+    required this.userName,
+    required this.email,
+    required this.passportExpiry,
+    required this.phoneNumber,
+  });
+
+  Map toJson() => {
+    'Email': email,
+    'FirstName': firstName,
+    'LastName': lastName,
+    'dateOfBirth': dateOfBirth,
+    'Gender': gender,
+    'Nationality': nationalityCode,
+    'PassportNumber': passportNumber,
+    'ExpiryDate': passportExpiry,
+    'IssueCountry': passportIssuerCountryCode,
+  };
+
+}
+
+UserDetails mapUserDetails(dynamic payload){
+  return UserDetails(
+    phoneNumber:payload["userName"]??"",
+    userName :payload["userName"]??"",
+    email :payload["email"]??"",
+    passportExpiry :payload["passportExpiry"] != null?DateTime.parse(payload["passportExpiry"]): DefaultInvalidDate,
+    gender: payload["gender"]??"",
+    firstName: payload["firstName"]??"",
+    lastName: payload["lastName"]??"",
+    phoneCountryCode: payload["phoneCountryCode"]??"",
+    imgUrl: payload["imgUrl"]??"",
+    passportNumber: payload["passportNumber"]??"",
+    dateOfBirth: payload["dateOfBirth"] != null?DateTime.parse(payload["dateOfBirth"]): DefaultInvalidDate,
+    passportIssuerCountryCode: payload["passportIssuerCountryCode"]??"",
+    passportIssuerCountryName: payload["passportIssuerCountryName"]??"",
+    nationalityCode:payload["nationalityCode"]??"",
+    nationalityName: payload["nationalityName"]??"",
+  );
+}
