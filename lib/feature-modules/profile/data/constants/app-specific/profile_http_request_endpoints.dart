641e995b46730b6613daf682fc8a07be4029edbd
Prasanth K M <prasanthkm49@gmail.com>
2023-10-18 09:11:47 +0530

    Added createcopax endpoint

--- lib/feature-modules/profile/data/constants/app-specific/profile_http_request_endpoints.dart
+++ lib/feature-modules/profile/data/constants/app-specific/profile_http_request_endpoints.dart
@@ -2,3 +2,4 @@ const String ProfileHttpRequestEndpointGetUserDetails = "/coreapi/api/Users/GetU
 const String ProfileHttpRequestEndpointGetUserTravelStories = "/coreapi/api/TravelStories/GetTravelStories";
 const String ProfileHttpRequestEndpointCreateUserTravelStory = "/coreapi/api/TravelStories/CreateTestimonials";
 const String ProfileHttpRequestEndpointGetUserCoPaxs = "/coreapi/api/CoPaxs/GetUserCoPaxs";
+const String ProfileHttpRequestEndpointGetUserCreateCoPaxs = "/coreapi/api/CoPaxs/CreateUserCopax";
