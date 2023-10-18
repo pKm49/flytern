d9a849088958d3b0efc094ea2e53d681644b2827
Prasanth K M <prasanthkm49@gmail.com>
2023-10-18 10:39:38 +0530

    added endpoint for update copax

--- lib/feature-modules/profile/data/constants/app-specific/profile_http_request_endpoints.dart
+++ lib/feature-modules/profile/data/constants/app-specific/profile_http_request_endpoints.dart
@@ -3,4 +3,5 @@ const String ProfileHttpRequestEndpointGetUserTravelStories = "/coreapi/api/Trav
 const String ProfileHttpRequestEndpointCreateUserTravelStory = "/coreapi/api/TravelStories/CreateTestimonials";
 const String ProfileHttpRequestEndpointGetUserCoPaxs = "/coreapi/api/CoPaxs/GetUserCoPaxs";
 const String ProfileHttpRequestEndpointGetUserCreateCoPaxs = "/coreapi/api/CoPaxs/CreateUserCopax";
+const String ProfileHttpRequestEndpointGetUserUpdateCoPaxs = "/coreapi/api/CoPaxs/UpdateUserCopax";
 const String ProfileHttpRequestEndpointGetUserDeleteCoPaxs = "/coreapi/api/CoPaxs/DeleteUserCopax/";
