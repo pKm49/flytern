0d27b8bdc943c62da07f40baf473dc7261e52370
Prasanth K M <prasanthkm49@gmail.com>
2023-10-18 20:12:48 +0530

    added update password workflow

--- lib/feature-modules/profile/ui/pages/profile_edit_pages/profile_reset_password_page.dart
+++ lib/feature-modules/profile/ui/pages/profile_edit_pages/profile_reset_password_page.dart
@@ -1,6 +1,7 @@
 
 import 'package:dotted_border/dotted_border.dart';
 import 'package:flutter/material.dart';
+import 'package:flytern/feature-modules/profile/controllers/profile_controller.dart';
 import 'package:flytern/shared/data/constants/ui_constants/asset_urls.dart';
 import 'package:flytern/shared/data/constants/ui_constants/style_params.dart';
 import 'package:flytern/shared/data/constants/ui_constants/widget_styles.dart';
@@ -19,8 +20,8 @@ class ProfileResetPasswordPage extends StatefulWidget {
 
 class _ProfileResetPasswordPageState extends State<ProfileResetPasswordPage> {
 
-  TextEditingController emailController = TextEditingController();
-  TextEditingController passwordController = TextEditingController();
+  final profileController = Get.find<ProfileController>();
+  final GlobalKey<FormState> resetPasswordFormKey = GlobalKey<FormState>();  
 
   @override
   Widget build(BuildContext context) {
@@ -33,78 +34,85 @@ class _ProfileResetPasswordPageState extends State<ProfileResetPasswordPage> {
           title: Text("reset_password".tr),
           elevation: 0.5,
         ),
-        body: Container(
-          width: screenwidth,
-          height: screenheight,
-          color: flyternGrey10,
-          child: ListView(
-            children: [
-              Container(
-                width: screenwidth  ,
-                padding: flyternLargePaddingAll,
-                height: flyternSpaceLarge,
-                decoration: BoxDecoration(
-                  color: flyternGrey10,
-                ),
-              ),
-              Container(
-                padding: flyternLargePaddingHorizontal.copyWith(top: flyternSpaceLarge,bottom: flyternSpaceMedium),
-                color: flyternBackgroundWhite,
-                child: TextFormField(
-                    controller: emailController,
-                    validator: (value) => checkIfEmailValid(value),
-                    keyboardType: TextInputType.emailAddress,
-                    decoration: InputDecoration(
-                      suffixIcon: Icon(Ionicons.eye_outline),
-                      labelText: "current_password".tr,
-                    )),
-              ),
-              Container(
-                padding: flyternLargePaddingHorizontal.copyWith(top: 0,bottom: flyternSpaceMedium),
-                color: flyternBackgroundWhite,
-                child: TextFormField(
-                    controller: emailController,
-                    validator: (value) => checkIfEmailValid(value),
-                    keyboardType: TextInputType.emailAddress,
-                    decoration: InputDecoration(
-                      suffixIcon: Icon(Ionicons.eye_outline),
-                      labelText: "set_new_password".tr,
-                    )),
-              ),
-              Container(
-                padding: flyternLargePaddingHorizontal.copyWith(top:0,bottom: flyternSpaceLarge),
-                color: flyternBackgroundWhite,
-                child: TextFormField(
-                    controller: emailController,
-                    validator: (value) => checkIfEmailValid(value),
-                    keyboardType: TextInputType.emailAddress,
-                    decoration: InputDecoration(
-                      suffixIcon: Icon(Ionicons.eye_outline),
-                      labelText: "confirm_password".tr,
-                    )),
-              ),
+        body: Form(
+          key: resetPasswordFormKey,
+          child: Obx(
+            ()=> Container(
+              width: screenwidth,
+              height: screenheight,
+              color: flyternGrey10,
+              child: ListView(
+                children: [
+                  Container(
+                    width: screenwidth  ,
+                    padding: flyternLargePaddingAll,
+                    height: flyternSpaceLarge,
+                    decoration: BoxDecoration(
+                      color: flyternGrey10,
+                    ),
+                  ), 
+                  Container(
+                    padding: flyternLargePaddingAll,
+                    color: flyternBackgroundWhite,
+                    child: TextFormField(
+                        controller: profileController.passwordController.value,
+                        validator: (value) => checkIfPasswordFieldValid(value),
+                        keyboardType: TextInputType.emailAddress,
+                        decoration: InputDecoration(
+                          suffixIcon: Icon(Ionicons.eye_outline),
+                          labelText: "set_new_password".tr,
+                        )),
+                  ),
+                  Container(
+                    padding: flyternLargePaddingHorizontal.copyWith(top:0,bottom: flyternSpaceLarge),
+                    color: flyternBackgroundWhite,
+                    child: TextFormField(
+                        controller: profileController.confirmPasswordController.value,
+                        validator: (value) => checkIfConfirmPasswordFieldValid(value,profileController.passwordController.value.text),
+                        keyboardType: TextInputType.emailAddress,
+                        decoration: InputDecoration(
+                          suffixIcon: Icon(Ionicons.eye_outline),
+                          labelText: "confirm_password".tr,
+                        )),
+                  ),
 
-              Container(
-                height: 70+(flyternSpaceSmall*2),
-                padding: flyternLargePaddingAll,
-              )
-            ],
+                  Container(
+                    height: 70+(flyternSpaceSmall*2),
+                    padding: flyternLargePaddingAll,
+                  )
+                ],
+              ),
+            ),
           ),
         ),
-        bottomSheet: Container(
-          width: screenwidth,
-          color: flyternBackgroundWhite,
-          height: 60 + (flyternSpaceSmall * 2),
-          padding: flyternLargePaddingAll.copyWith(
-              top: flyternSpaceSmall, bottom: flyternSpaceSmall),
-          child: Center(
-            child: SizedBox(
-              width: double.infinity,
-              child: ElevatedButton(style: getElevatedButtonStyle(context),
-                  onPressed: () {
-                    Navigator.pop(context);
-                  },
-                  child: Text("update".tr)),
+        bottomSheet: Obx(
+          ()=> Container(
+            width: screenwidth,
+            color: flyternBackgroundWhite,
+            height: 60 + (flyternSpaceSmall * 2),
+            padding: flyternLargePaddingAll.copyWith(
+                top: flyternSpaceSmall, bottom: flyternSpaceSmall),
+            child: Center(
+              child: SizedBox(
+                width: double.infinity,
+                child: ElevatedButton(style: getElevatedButtonStyle(context),
+                    onPressed: () {
+                      if (resetPasswordFormKey.currentState!.validate() &&
+                          !profileController.isPasswordSubmitting.value) {
+                        FocusManager.instance.primaryFocus?.unfocus();
+                        profileController.updatePassword( );
+                      }
+                    },
+                    child:  profileController.isPasswordSubmitting.value
+                        ? const SizedBox(
+                      width: 16,
+                      height: 16,
+                      child: CircularProgressIndicator(
+                        color: flyternBackgroundWhite,
+                      ),
+                    )
+                        :Text("update".tr)),
+              ),
             ),
           ),
         )
