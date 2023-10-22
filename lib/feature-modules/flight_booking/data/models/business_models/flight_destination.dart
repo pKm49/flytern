f73940298c8d68d5aab8772af3d06eecbd0c2756
Prasanth K M <prasanthkm49@gmail.com>
2023-10-22 14:18:55 +0530

    created model for flight destinations

---
+++ lib/feature-modules/flight_booking/data/models/business_models/flight_destination.dart
@@ -0,0 +1,39 @@
+class FlightDestination {
+
+  final String airportCode;
+  final String airportName;
+  final String uniqueCombination;
+  final int sort;
+  final String countryISOCode; 
+  final String flag;
+
+  FlightDestination({
+    required this.airportCode,
+    required this.airportName, 
+    required this.uniqueCombination,
+    required this.sort,
+    required this.countryISOCode,
+    required this.flag,
+  });
+
+  Map toJson() => {
+    'airportCode': airportCode,
+    'airportName': airportName,
+    'uniqueCombination': uniqueCombination,
+    'sort': sort,
+    'countryISOCode': countryISOCode,
+    'flag': flag,
+  };
+
+}
+
+FlightDestination mapFlightDestination(dynamic payload){
+  return FlightDestination(
+    airportCode :payload["airportCode"]??"",
+    airportName :payload["airportName"]??"",
+    uniqueCombination :payload["uniqueCombination"]??"",
+    sort :payload["sort"]??-1,
+    countryISOCode :payload["countryISOCode"]??"",
+    flag :payload["flag"]??"",
+  );
+}
