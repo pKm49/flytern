a74e1f1afb679866bf20c515215636eae355ca7a
Prasanth K M <prasanthkm49@gmail.com>
2023-10-22 14:25:58 +0530

    added request method for geting flight destinations on query

--- lib/feature-modules/flight_booking/services/http-services/flight_booking_http_services.dart
+++ lib/feature-modules/flight_booking/services/http-services/flight_booking_http_services.dart
@@ -1,5 +1,6 @@
 import 'package:flytern/feature-modules/flight_booking/data/constants/app_specific/flight_booking_http_request_endpoints.dart';
 import 'package:flytern/feature-modules/flight_booking/data/models/business_models/explore_data.dart';
+import 'package:flytern/feature-modules/flight_booking/data/models/business_models/flight_destination.dart';
 import 'package:flytern/shared/data/constants/app_specific/shared_http_request_endpoints.dart';
 import 'package:flytern/shared/data/models/app_specific/flytern_http_response.dart';
 import 'package:flytern/shared/data/models/app_specific/set_device_info_request_body.dart';
@@ -23,5 +24,19 @@ class FlightBookingHttpService {
     return null;
   }
 
+  Future<List<FlightDestination>> getFlightDestinations(String searchQuery ) async {
+    FlyternHttpResponse response = await getRequest(
+        FlightBookingHttpRequestEndpointGetDestinations+searchQuery, null);
+    List<FlightDestination> flightDestinations =[];
+    if(response.success){
+      if(response.data != null){
+        for(var i =0; i<response.data.length;i++) {
+          flightDestinations.add(mapFlightDestination(response.data[i]));
+        }
+      }
+    }
+
+    return flightDestinations;
+  }
 
 }
