03b340a04a957e4f593fda146a8b1dc09eb2f09c
Prasanth K M <prasanthkm49@gmail.com>
2023-10-22 14:04:06 +0530

    removed unwanted imports

--- lib/feature-modules/flight_booking/controllers/flight_booking_controller.dart
+++ lib/feature-modules/flight_booking/controllers/flight_booking_controller.dart
@@ -4,12 +4,6 @@ import 'package:flytern/feature-modules/flight_booking/data/models/business_mode
 import 'package:flytern/feature-modules/flight_booking/data/models/business_models/recommended_package.dart';
 import 'package:flytern/feature-modules/flight_booking/data/models/business_models/travel_story.dart';
 import 'package:flytern/feature-modules/flight_booking/services/http-services/flight_booking_http_services.dart';
-import 'package:flytern/shared/data/constants/business_constants/available_countries.dart';
-import 'package:flytern/shared/data/constants/business_constants/available_languages.dart';
-import 'package:flytern/shared/data/models/business_models/country.dart';
-import 'package:flytern/shared/data/models/business_models/language.dart';
-import 'package:flytern/shared/data/models/business_models/support_info.dart';
-import 'package:flytern/shared/services/http-services/shared_http_services.dart';
 import 'package:get/get.dart';
 
 class FlightBookingController extends GetxController {
