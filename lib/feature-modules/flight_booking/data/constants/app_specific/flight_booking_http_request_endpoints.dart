d02e867e604d12fb4355cfd5e63be714a7360cfd
Prasanth K M <prasanthkm49@gmail.com>
2023-10-22 14:20:48 +0530

    added endpoints for flight related results

--- lib/feature-modules/flight_booking/data/constants/app_specific/flight_booking_http_request_endpoints.dart
+++ lib/feature-modules/flight_booking/data/constants/app_specific/flight_booking_http_request_endpoints.dart
@@ -1,2 +1,7 @@
 const String FlightBookingHttpRequestEndpointGetInitalInfo = "/coreapi/api/Explores/GetExplores";
+const String FlightBookingHttpRequestEndpointGetDestinations = "/coreapi/api/Flights/GetDestination?search";
+const String FlightBookingHttpRequestEndpointGetMoreOptionFlights = "/coreapi/api/Flights/MoreOptionFlights";
+const String FlightBookingHttpRequestEndpointFilterFlights = "/coreapi/api/Flights/FilterFlights";
+const String FlightBookingHttpRequestEndpointSearchFlights = "/coreapi/api/Flights/SearchFlights";
+const String FlightBookingHttpRequestEndpointGetDetailFlights = "/coreapi/api/Flights/DetailFlights";
 
