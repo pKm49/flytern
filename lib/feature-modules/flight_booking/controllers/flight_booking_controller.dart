0c8522a30ac2c04810149d193e86f1e62bc266a6
Prasanth K M <prasanthkm49@gmail.com>
2023-10-25 06:14:01 +0530

    added workflow for flight search

--- lib/feature-modules/flight_booking/controllers/flight_booking_controller.dart
+++ lib/feature-modules/flight_booking/controllers/flight_booking_controller.dart
@@ -1,5 +1,8 @@
 import 'package:flytern/feature-modules/flight_booking/data/models/business_models/cabin_class.dart';
 import 'package:flytern/feature-modules/flight_booking/data/models/business_models/explore_data.dart';
+import 'package:flytern/feature-modules/flight_booking/data/models/business_models/flight_destination.dart';
+import 'package:flytern/feature-modules/flight_booking/data/models/business_models/flight_search_data.dart';
+import 'package:flytern/feature-modules/flight_booking/data/models/business_models/flight_search_item.dart';
 import 'package:flytern/feature-modules/flight_booking/data/models/business_models/popular_destination.dart';
 import 'package:flytern/feature-modules/flight_booking/data/models/business_models/recommended_package.dart';
 import 'package:flytern/feature-modules/flight_booking/data/models/business_models/travel_story.dart';
@@ -7,7 +10,7 @@ import 'package:flytern/feature-modules/flight_booking/services/http-services/fl
 import 'package:get/get.dart';
 
 class FlightBookingController extends GetxController {
-
+  var isFlightDestinationsLoading = true.obs;
   var isInitialDataLoading = true.obs;
   var flightBookingHttpService = FlightBookingHttpService();
 
@@ -15,6 +18,9 @@ class FlightBookingController extends GetxController {
   var recommendedPackages = <RecommendedPackage>[].obs;
   var popularDestinations = <PopularDestination>[].obs;
   var travelStories = <TravelStory>[].obs;
+  var flightDestinations = <FlightDestination>[].obs;
+  var flightSearchItems = <FlightSearchItem>[].obs;
+  var flightSearchData = getDefaultFlightSearchData().obs;
 
   @override
   void onInit() {
@@ -23,11 +29,10 @@ class FlightBookingController extends GetxController {
   }
 
   Future<void> getInitialInfo() async {
-
     ExploreData? exploreData = await flightBookingHttpService.getInitialInfo();
     print("getInitialInfo completed");
     print(exploreData != null);
-    if(exploreData != null){
+    if (exploreData != null) {
       cabinClasses.value = exploreData.cabinClasses;
       recommendedPackages.value = exploreData.recommendedPackages;
       popularDestinations.value = exploreData.popularDestinations;
@@ -35,7 +40,51 @@ class FlightBookingController extends GetxController {
     }
 
     isInitialDataLoading.value = false;
-
   }
+
+  Future<List<FlightDestination>> getFlightDestinations(
+      String searchQuery) async {
+    flightDestinations.value =
+    await flightBookingHttpService.getFlightDestinations(searchQuery);
+    isFlightDestinationsLoading.value = false;
+    return flightDestinations.value;
+  }
+
+  setDestination(FlightDestination flightDestination, bool isArrival,
+      int index) {
+    FlightSearchData newFlightSearchData = FlightSearchData(
+        promoCode: flightSearchData.value.promoCode,
+        adults: flightSearchData.value.adults,
+        child: flightSearchData.value.child,
+        infants: flightSearchData.value.infants,
+        searchList: getUpdatedSearchList(flightDestination, isArrival, index),
+        allowedCabins: flightSearchData.value.allowedCabins,
+        mode: flightSearchData.value.mode,
+        isDirectFlight: flightSearchData.value.isDirectFlight);
+
+    flightSearchData.value = newFlightSearchData;
+  }
+
+  List<FlightSearchItem> getUpdatedSearchList(
+      FlightDestination flightDestination, bool isArrival, int index) {
+    List<FlightSearchItem> flightSearchItems = [];
+
+
+    for (var i = 0; i < flightSearchData.value.searchList.length; i++) {
+      if (index != i) {
+        flightSearchItems.add(flightSearchData.value.searchList[i]);
+      } else {
+        FlightSearchItem flightSearchItem = FlightSearchItem(
+            departure: !isArrival ? flightDestination : flightSearchData.value.searchList[i].departure ,
+            arrival: isArrival ? flightDestination : flightSearchData.value .searchList[i].arrival,
+            departureDate: flightSearchData.value.searchList[i].departureDate,
+            returnDate: flightSearchData.value.searchList[i].returnDate
+        );
+        flightSearchItems.add(flightSearchItem);
+      }
+    }
+    return flightSearchItems;
+  }
+
 
 }
