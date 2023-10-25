17a08e1f416d1980d1f7b022cb15efcca40ae5d3
Prasanth K M <prasanthkm49@gmail.com>
2023-10-25 12:43:53 +0530

    added methods for seting mode, add & remove searchitem

--- lib/feature-modules/flight_booking/services/helper-services/flight_booking_helper_services.dart
+++ lib/feature-modules/flight_booking/services/helper-services/flight_booking_helper_services.dart
@@ -1,8 +1,10 @@
+import 'package:flytern/feature-modules/flight_booking/data/constants/business_specific/flight_mode.dart';
 import 'package:flytern/feature-modules/flight_booking/data/models/business_models/flight_destination.dart';
 import 'package:flytern/feature-modules/flight_booking/data/models/business_models/flight_search_data.dart';
 import 'package:flytern/feature-modules/flight_booking/data/models/business_models/flight_search_item.dart';
 
 class FlightBookingHelperServices{
+
   List<FlightSearchItem> getUpdatedSearchList(FlightSearchData flightSearchData,
       FlightDestination flightDestination, bool isArrival, int index) {
     List<FlightSearchItem> flightSearchItems = [];
@@ -24,5 +26,61 @@ class FlightBookingHelperServices{
     return flightSearchItems;
   }
 
+  FlightSearchData setFlightMode(FlightSearchData flightSearchData,FlightMode newMode){
 
+    return FlightSearchData(
+        promoCode: flightSearchData.promoCode,
+        adults: flightSearchData.adults,
+        child: flightSearchData.child,
+        infants: flightSearchData.infants,
+        searchList: flightSearchData.searchList,
+        allowedCabins: flightSearchData.allowedCabins,
+        mode: newMode,
+        isDirectFlight: flightSearchData.isDirectFlight);
+
+  }
+
+  FlightSearchData addFlightSearchItem(FlightSearchData flightSearchData ){
+
+    List<FlightSearchItem> flightSearchItems = flightSearchData.searchList;
+
+    FlightSearchItem flightSearchItem = FlightSearchItem(
+        departure: flightSearchData.searchList[flightSearchData.searchList.length-1].arrival ,
+        arrival: flightSearchData.searchList[flightSearchData.searchList.length-1].departure,
+        departureDate: DateTime.now(),
+        returnDate:DateTime.now()
+    );
+    flightSearchItems.add(flightSearchItem);
+    return FlightSearchData(
+        promoCode: flightSearchData.promoCode,
+        adults: flightSearchData.adults,
+        child: flightSearchData.child,
+        infants: flightSearchData.infants,
+        searchList: flightSearchItems,
+        allowedCabins: flightSearchData.allowedCabins,
+        mode: flightSearchData.mode,
+        isDirectFlight: flightSearchData.isDirectFlight);
+
+  }
+
+  FlightSearchData removeFlightSearchItem(FlightSearchData flightSearchData, int index ){
+
+    List<FlightSearchItem> flightSearchItems = flightSearchData.searchList;
+
+    for (var i = 0; i < flightSearchData.searchList.length; i++) {
+      if (index != i) {
+        flightSearchItems.add(flightSearchData.searchList[i]);
+      }
+    }
+    return FlightSearchData(
+        promoCode: flightSearchData.promoCode,
+        adults: flightSearchData.adults,
+        child: flightSearchData.child,
+        infants: flightSearchData.infants,
+        searchList: flightSearchItems,
+        allowedCabins: flightSearchData.allowedCabins,
+        mode: flightSearchData.mode,
+        isDirectFlight: flightSearchData.isDirectFlight);
+
+  }
 }
