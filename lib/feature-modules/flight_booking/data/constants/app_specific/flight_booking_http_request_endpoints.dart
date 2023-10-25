c4e9f7271ad253277cd76cb347e2a8b206d66f37
Prasanth K M <prasanthkm49@gmail.com>
2023-10-25 06:13:19 +0530

    removed params variable

--- lib/feature-modules/flight_booking/data/constants/app_specific/flight_booking_http_request_endpoints.dart
+++ lib/feature-modules/flight_booking/data/constants/app_specific/flight_booking_http_request_endpoints.dart
@@ -1,5 +1,5 @@
 const String FlightBookingHttpRequestEndpointGetInitalInfo = "/coreapi/api/Explores/GetExplores";
-const String FlightBookingHttpRequestEndpointGetDestinations = "/coreapi/api/Flights/GetDestination?search";
+const String FlightBookingHttpRequestEndpointGetDestinations = "/coreapi/api/Flights/GetDestination";
 const String FlightBookingHttpRequestEndpointGetMoreOptionFlights = "/coreapi/api/Flights/MoreOptionFlights";
 const String FlightBookingHttpRequestEndpointFilterFlights = "/coreapi/api/Flights/FilterFlights";
 const String FlightBookingHttpRequestEndpointSearchFlights = "/coreapi/api/Flights/SearchFlights";
