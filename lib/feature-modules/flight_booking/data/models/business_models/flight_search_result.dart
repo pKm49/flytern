fc90f25b9e93fd5c9b0db7627e33975db2507c73
Prasanth K M <prasanthkm49@gmail.com>
2023-10-27 21:22:36 +0530

    added model for flight search results

---
+++ lib/feature-modules/flight_booking/data/models/business_models/flight_search_result.dart
@@ -0,0 +1,27 @@
+import 'package:flytern/feature-modules/flight_booking/data/constants/business_specific/flight_mode.dart';
+import 'package:flytern/feature-modules/flight_booking/data/models/business_models/flight_allowed_cabin.dart';
+import 'package:flytern/feature-modules/flight_booking/data/models/business_models/flight_destination.dart';
+import 'package:flytern/feature-modules/flight_booking/data/models/business_models/flight_search_item.dart';
+import 'package:flytern/feature-modules/flight_booking/data/models/business_models/flight_search_response.dart';
+import 'package:flytern/feature-modules/flight_booking/data/models/business_models/flight_search_response_dto_segment.dart';
+import 'package:flytern/feature-modules/flight_booking/data/models/business_models/sorting_dcs.dart';
+
+class FlightSearchResult {
+
+  final List<FlightSearchResponse> searchResponses;
+  final List<SortingDcs> sortingDcs;
+  final List<SortingDcs> airlineDcs;
+  final List<SortingDcs> departureTimeDcs;
+  final List<SortingDcs> arrivalTimeDcs;
+  final List<SortingDcs> stopDcs;
+
+  FlightSearchResult({
+   required this.searchResponses,
+   required this.sortingDcs,
+   required this.airlineDcs,
+   required this.departureTimeDcs,
+   required this.arrivalTimeDcs,
+   required this.stopDcs
+  });
+
+}
