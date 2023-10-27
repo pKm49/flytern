c371895a83b61fda87ab6a378cba7f739a6c8701
Prasanth K M <prasanthkm49@gmail.com>
2023-10-27 15:42:47 +0530

    added getFLightSearch results request

--- lib/feature-modules/flight_booking/services/http-services/flight_booking_http_services.dart
+++ lib/feature-modules/flight_booking/services/http-services/flight_booking_http_services.dart
@@ -1,6 +1,8 @@
 import 'package:flytern/feature-modules/flight_booking/data/constants/app_specific/flight_booking_http_request_endpoints.dart';
 import 'package:flytern/feature-modules/flight_booking/data/models/business_models/explore_data.dart';
 import 'package:flytern/feature-modules/flight_booking/data/models/business_models/flight_destination.dart';
+import 'package:flytern/feature-modules/flight_booking/data/models/business_models/flight_search_data.dart';
+import 'package:flytern/feature-modules/flight_booking/data/models/business_models/flight_search_response.dart';
 import 'package:flytern/shared/data/constants/app_specific/shared_http_request_endpoints.dart';
 import 'package:flytern/shared/data/models/app_specific/flytern_http_response.dart';
 import 'package:flytern/shared/data/models/app_specific/set_device_info_request_body.dart';
@@ -38,6 +40,21 @@ class FlightBookingHttpService {
     }
 
     return flightDestinations;
+  }
+
+  Future<List<FlightSearchResponse>> getFlightSearchResults(FlightSearchData flightSearchData ) async {
+    FlyternHttpResponse response = await postRequest(
+        FlightBookingHttpRequestEndpointSearchFlights, flightSearchData.toJson());
+    List<FlightSearchResponse> flightSearchResponses =[];
+    if(response.success){
+      if(response.data != null && response.data["searchResponses"] != null ){
+        for(var i =0; i<response.data["searchResponses"].length;i++) {
+          flightSearchResponses.add(mapFlightSearchResponse(response.data["searchResponses"][i]));
+        }
+      }
+    }
+
+    return flightSearchResponses;
   }
 
 }
