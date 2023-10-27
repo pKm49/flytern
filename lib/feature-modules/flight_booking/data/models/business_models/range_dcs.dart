011686243185d8ad74e8a17390015be9cad71c82
Prasanth K M <prasanthkm49@gmail.com>
2023-10-27 21:22:22 +0530

    added model for range values for flight search

---
+++ lib/feature-modules/flight_booking/data/models/business_models/range_dcs.dart
@@ -0,0 +1,19 @@
+class RangeDcs {
+
+  final double min;
+  final double max;
+  RangeDcs({
+    required this.min, 
+    required this.max
+  });
+
+
+
+}
+
+RangeDcs mapRangeDcs(dynamic payload){
+  return RangeDcs(
+    min :payload["min"]??0.0,
+    max :payload["max"]??500.0,
+  );
+}
