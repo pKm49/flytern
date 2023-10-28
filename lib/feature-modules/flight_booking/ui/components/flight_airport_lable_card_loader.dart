19cd24f867a77b5f480303aab235884604cc33d0
Prasanth K M <prasanthkm49@gmail.com>
2023-10-28 12:01:52 +0530

    added loader screen for label card

---
+++ lib/feature-modules/flight_booking/ui/components/flight_airport_lable_card_loader.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+import 'package:flytern/shared/data/constants/ui_constants/style_params.dart';
+import 'package:flytern/shared/services/utility-services/widget_generator.dart';
+import 'package:flytern/shared/services/utility-services/widget_properties_generator.dart';
+import 'package:get/get.dart';
+import 'package:shimmer/shimmer.dart';
+
+class FlightAirportLabelCardLoader extends StatelessWidget {
+
+  int sideNumber;
+  FlightAirportLabelCardLoader({super.key,
+      required this.sideNumber, });
+
+  @override
+  Widget build(BuildContext context) {
+    return Wrap(
+      direction: Axis.vertical,
+      alignment: WrapAlignment.start,
+      crossAxisAlignment:sideNumber==1? WrapCrossAlignment.start: WrapCrossAlignment.end,
+      children: [
+
+        Shimmer.fromColors(
+          baseColor: flyternBackgroundWhite,
+          highlightColor: flyternGrey20,
+          child: Container(
+            decoration: BoxDecoration(
+              color:flyternGrey10,
+              borderRadius:
+              BorderRadius.circular(flyternBorderRadiusExtraSmall),
+            ),
+            height: 20,
+            width: 60,
+          ),
+        ),
+        addVerticalSpace(flyternSpaceExtraSmall),
+        Shimmer.fromColors(
+          baseColor: flyternBackgroundWhite,
+          highlightColor: flyternGrey20,
+          child: Container(
+            decoration: BoxDecoration(
+              color:flyternGrey10,
+              borderRadius:
+              BorderRadius.circular(flyternBorderRadiusExtraSmall),
+            ),
+            height: 60,
+            width: 80,
+          ),
+        ),
+        addVerticalSpace(flyternSpaceExtraSmall),
+
+        Shimmer.fromColors(
+          baseColor: flyternBackgroundWhite,
+          highlightColor: flyternGrey20,
+          child: Container(
+            decoration: BoxDecoration(
+              color:flyternGrey10,
+              borderRadius:
+              BorderRadius.circular(flyternBorderRadiusExtraSmall),
+            ),
+            height: 20,
+            width: 50,
+          ),
+        ),
+      ],
+    );
+  }
+}
