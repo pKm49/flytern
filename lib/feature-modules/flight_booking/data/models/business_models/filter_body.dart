5e9132b4353a664353965419fcdb177a29cbd48d
Prasanth K M <prasanthkm49@gmail.com>
2023-10-28 16:39:54 +0530

    added model for filter body

---
+++ lib/feature-modules/flight_booking/data/models/business_models/filter_body.dart
@@ -0,0 +1,35 @@
+class FilterBody {
+
+  final int pageId;
+  final int objectID;
+  final String priceMinMaxDc;
+  final String arrivalTimeDc;
+  final String departureTimeDc;
+  final String airlineDc;
+  final String stopDc;
+  final String sortingDc;
+
+  FilterBody({
+    required this.pageId,
+    required this.objectID,
+    required this.priceMinMaxDc,
+    required this.arrivalTimeDc,
+    required this.departureTimeDc,
+    required this.airlineDc,
+    required this.stopDc,
+    required this.sortingDc
+  });
+
+  Map toJson() => {
+    'pageId': pageId,
+    'objectID': objectID,
+    'priceMinMaxDc': priceMinMaxDc,
+    'arrivalTimeDc': arrivalTimeDc,
+    'departureTimeDc': departureTimeDc,
+    'airlineDc': airlineDc,
+    'stopDc': stopDc,
+    'sortingDc': sortingDc,
+  };
+
+}
+
