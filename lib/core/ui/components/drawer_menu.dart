acf7e371fe2b6f4d8b14505aeb750817a5a2c485
Prasanth K M <prasanthkm49@gmail.com>
2023-11-04 22:36:39 +0530

    rating feature added

--- lib/core/ui/components/drawer_menu.dart
+++ lib/core/ui/components/drawer_menu.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flytern/core/controllers/core_controller.dart';
@@ -11,6 +13,7 @@ import 'package:flytern/shared/ui/components/confirm_dialogue.dart';
 import 'package:flytern/shared/ui/components/preposticon_button.dart';
 import 'package:get/get.dart';
 import 'package:ionicons/ionicons.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class CoreDrawerMenuPage extends StatefulWidget {
   const CoreDrawerMenuPage({super.key});
@@ -33,185 +36,204 @@ class _CoreDrawerMenuPageState extends State<CoreDrawerMenuPage> {
     return Container(
       padding: flyternLargePaddingAll,
       color: flyternBackgroundWhite,
-      child: ListView(
-        children: [
-          addVerticalSpace(flyternSpaceLarge),
-          SizedBox(
-            width: double.infinity,
-            child: PrePostIconButton(
-              specialColor:0,
-              onPressed: (){
-                Get.toNamed(Approute_coreSmartPayment);
-              },
-              theme: 'dark',
-              border: 'bottom',
-              buttonTitle: "smart_payment".tr,
-              preIconData: Ionicons.cash_outline,
-              postIconData: Ionicons.chevron_forward,
-            ),
-          ),
-          addVerticalSpace(flyternSpaceSmall),
-          SizedBox(
-            width: double.infinity,
-            child: PrePostIconButton(
-              specialColor:0,
-              onPressed: (){
-                Get.toNamed(Approute_insuranceLandingPage);
-
-              },
-              theme: 'dark',
-              border: 'bottom',
-              buttonTitle: "travel_insurance".tr,
-              preIconData: Ionicons.pulse_outline,
-              postIconData: Ionicons.chevron_forward,
-            ),
-          ),
-          addVerticalSpace(flyternSpaceSmall),
-          SizedBox(
-            width: double.infinity,
-            child: PrePostIconButton(
-              specialColor:0,
-              onPressed: (){
-              },
-              theme: 'dark',
-              border: 'bottom',
-              buttonTitle: "extra_miles".tr,
-              preIconData: Ionicons.walk_outline,
-              postIconData: Ionicons.chevron_forward,
-            ),
-          ),
-          addVerticalSpace(flyternSpaceSmall),
-          SizedBox(
-            width: double.infinity,
-            child: PrePostIconButton(
-              specialColor:0,
-              onPressed: (){
-              },
-              theme: 'dark',
-              border: 'bottom',
-              buttonTitle: "gift_cards".tr,
-              preIconData: Ionicons.gift_outline,
-              postIconData: Ionicons.chevron_forward,
-            ),
-          ),
-
-          addVerticalSpace(flyternSpaceSmall),
-          SizedBox(
-            width: double.infinity,
-            child: PrePostIconButton(
-              specialColor:0,
-              onPressed: (){
-              },
-              theme: 'dark',
-              border: 'bottom',
-              buttonTitle: "travel_tips".tr,
-              preIconData: Ionicons.trail_sign_outline,
-              postIconData: Ionicons.chevron_forward,
-            ),
-          ),
-
-          addVerticalSpace(flyternSpaceSmall),
-          SizedBox(
-            width: double.infinity,
-            child: PrePostIconButton(
-              specialColor:0,
-              onPressed: (){
-                Get.toNamed(Approute_coreAppSettings);
-
-              },
-              theme: 'dark',
-              border: 'bottom',
-              buttonTitle: "app_settings".tr,
-              preIconData: Ionicons.settings_outline,
-              postIconData: Ionicons.chevron_forward,
-            ),
-          ),
-          addVerticalSpace(flyternSpaceSmall),
-          SizedBox(
-            width: double.infinity,
-            child: PrePostIconButton(
-              specialColor:0,
-              onPressed: (){
-                Get.toNamed(Approute_coreAppInfo);
-              },
-              theme: 'dark',
-              border: 'bottom',
-              buttonTitle: "info".tr,
-              preIconData: Ionicons.information_circle_outline,
-              postIconData: Ionicons.chevron_forward,
-            ),
-          ),
-          addVerticalSpace(flyternSpaceSmall),
-          SizedBox(
-            width: double.infinity,
-            child: PrePostIconButton(
-              specialColor:0,
-              onPressed: (){
-              },
-              theme: 'dark',
-              border: profileController.userDetails.value.email !=""?'bottom':'',
-              buttonTitle: "rating".tr,
-              preIconData: Ionicons.star_outline,
-              postIconData: Ionicons.chevron_forward,
-            ),
-          ),
-
-          addVerticalSpace(flyternSpaceSmall),
-          Visibility(
-            visible: profileController.userDetails.value.email !="",
-            child: SizedBox(
+      child: Obx(
+        ()=> ListView(
+          children: [
+            addVerticalSpace(flyternSpaceLarge),
+            SizedBox(
               width: double.infinity,
               child: PrePostIconButton(
-                  specialColor:1,
+                specialColor:0,
                 onPressed: (){
-                  showConfirmDialog();
+                  Get.toNamed(Approute_coreSmartPayment);
                 },
                 theme: 'dark',
-                border: '',
-                buttonTitle: "logout".tr,
-                preIconData: Ionicons.log_out_outline,
+                border: 'bottom',
+                buttonTitle: "smart_payment".tr,
+                preIconData: Ionicons.cash_outline,
                 postIconData: Ionicons.chevron_forward,
               ),
             ),
-          ),
+            addVerticalSpace(flyternSpaceSmall),
+            SizedBox(
+              width: double.infinity,
+              child: PrePostIconButton(
+                specialColor:0,
+                onPressed: (){
+                  Get.toNamed(Approute_insuranceLandingPage);
 
-          Visibility(
-            visible: profileController.userDetails.value.email =="",
-            child: Container(
-              padding: flyternLargePaddingVertical ,
-              color: flyternBackgroundWhite,
-              height: screenheight*.2,
-              child: Row(
-               crossAxisAlignment: CrossAxisAlignment.end,
-               children: [
-                 Expanded(
-                   child: ElevatedButton(
-                     child:   Text("sign_in".tr),
-                     onPressed: () async {
-                       Get.toNamed(Approute_login,
-                           arguments: [true]);
-                     },
-                     style: getElevatedButtonStyle(context).copyWith(
-                         backgroundColor: MaterialStateProperty.all<Color>(flyternSecondaryColor)
-                     ),),
-                 ),
-                 SizedBox(height: flyternSpaceMedium,width: 20,),
-                 Expanded(
-                   child: ElevatedButton(style: getElevatedButtonStyle(context),
+                },
+                theme: 'dark',
+                border: 'bottom',
+                buttonTitle: "travel_insurance".tr,
+                preIconData: Ionicons.pulse_outline,
+                postIconData: Ionicons.chevron_forward,
+              ),
+            ),
+            addVerticalSpace(flyternSpaceSmall),
+            SizedBox(
+              width: double.infinity,
+              child: PrePostIconButton(
+                specialColor:0,
+                onPressed: (){
+                },
+                theme: 'dark',
+                border: 'bottom',
+                buttonTitle: "extra_miles".tr,
+                preIconData: Ionicons.walk_outline,
+                postIconData: Ionicons.chevron_forward,
+              ),
+            ),
+            addVerticalSpace(flyternSpaceSmall),
+            SizedBox(
+              width: double.infinity,
+              child: PrePostIconButton(
+                specialColor:0,
+                onPressed: (){
+                },
+                theme: 'dark',
+                border: 'bottom',
+                buttonTitle: "gift_cards".tr,
+                preIconData: Ionicons.gift_outline,
+                postIconData: Ionicons.chevron_forward,
+              ),
+            ),
+
+            addVerticalSpace(flyternSpaceSmall),
+            SizedBox(
+              width: double.infinity,
+              child: PrePostIconButton(
+                specialColor:0,
+                onPressed: (){
+                },
+                theme: 'dark',
+                border: 'bottom',
+                buttonTitle: "travel_tips".tr,
+                preIconData: Ionicons.trail_sign_outline,
+                postIconData: Ionicons.chevron_forward,
+              ),
+            ),
+
+            addVerticalSpace(flyternSpaceSmall),
+            SizedBox(
+              width: double.infinity,
+              child: PrePostIconButton(
+                specialColor:0,
+                onPressed: (){
+                  Get.toNamed(Approute_coreAppSettings);
+
+                },
+                theme: 'dark',
+                border: 'bottom',
+                buttonTitle: "app_settings".tr,
+                preIconData: Ionicons.settings_outline,
+                postIconData: Ionicons.chevron_forward,
+              ),
+            ),
+            addVerticalSpace(flyternSpaceSmall),
+            SizedBox(
+              width: double.infinity,
+              child: PrePostIconButton(
+                specialColor:0,
+                onPressed: (){
+                  Get.toNamed(Approute_coreAppInfo);
+                },
+                theme: 'dark',
+                border: 'bottom',
+                buttonTitle: "info".tr,
+                preIconData: Ionicons.information_circle_outline,
+                postIconData: Ionicons.chevron_forward,
+              ),
+            ),
+            addVerticalSpace(flyternSpaceSmall),
+            SizedBox(
+              width: double.infinity,
+              child: PrePostIconButton(
+                specialColor:0,
+                onPressed: (){
+                  launchRatingPage();
+                },
+                theme: 'dark',
+                border: profileController.userDetails.value.email !=""?'bottom':'',
+                buttonTitle: "rating".tr,
+                preIconData: Ionicons.star_outline,
+                postIconData: Ionicons.chevron_forward,
+              ),
+            ),
+
+            addVerticalSpace(flyternSpaceSmall),
+            Visibility(
+              visible: profileController.userDetails.value.email !="",
+              child: SizedBox(
+                width: double.infinity,
+                child: PrePostIconButton(
+                    specialColor:1,
+                  onPressed: (){
+                    showConfirmDialog();
+                  },
+                  theme: 'dark',
+                  border: '',
+                  buttonTitle: "logout".tr,
+                  preIconData: Ionicons.log_out_outline,
+                  postIconData: Ionicons.chevron_forward,
+                ),
+              ),
+            ),
+
+            Visibility(
+              visible: profileController.userDetails.value.email =="",
+              child: Container(
+                padding: flyternLargePaddingVertical ,
+                color: flyternBackgroundWhite,
+                height: screenheight*.2,
+                child: Row(
+                 crossAxisAlignment: CrossAxisAlignment.end,
+                 children: [
+                   Expanded(
+                     child: ElevatedButton(
+                       child:   Text("sign_in".tr),
                        onPressed: () async {
-                         Get.toNamed(Approute_registerPersonalData);
+                         Get.toNamed(Approute_login,
+                             arguments: [true]);
                        },
-                       child:Text("create_account".tr )),
-                 ),
-               ],
-                  ),
-            )
-          ),
+                       style: getElevatedButtonStyle(context).copyWith(
+                           backgroundColor: MaterialStateProperty.all<Color>(flyternSecondaryColor)
+                       ),),
+                   ),
+                   SizedBox(height: flyternSpaceMedium,width: 20,),
+                   Expanded(
+                     child: ElevatedButton(style: getElevatedButtonStyle(context),
+                         onPressed: () async {
+                           Get.toNamed(Approute_registerPersonalData);
+                         },
+                         child:Text("create_account".tr )),
+                   ),
+                 ],
+                    ),
+              )
+            ),
 
-        ],
+          ],
+        ),
       ),
     );
   }
+  Future<void> launchRatingPage( ) async {
+
+    if (Platform.isAndroid || Platform.isIOS) {
+      final appId = Platform.isAndroid ? 'com.facebook.katana' : 'id284882215';
+      final url = Uri.parse(
+        Platform.isAndroid
+            ? "market://details?id=$appId"
+            : "https://apps.apple.com/app/id$appId",
+      );
+      launchUrl(
+        url,
+        mode: LaunchMode.externalApplication,
+      );
+    }
+  }
+
 
   showConfirmDialog() async {
 
